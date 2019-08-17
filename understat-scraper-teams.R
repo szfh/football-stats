3e59f655bcc39005b010f462b69dab6927c0c8b2
saintsnumbers <52595717+saintsnumbers@users.noreply.github.com>
2019-08-17 11:50:06 +0100

    better team_meta function

--- understat-scraper-teams.R
+++ understat-scraper-teams.R
@@ -1,38 +1,38 @@
-# get_league_teams_stats("EPL",2018) %>%
-#   View("EPL 2018")
-
-get_team_players_stats("Southampton",2018) %>%
-  View("Southampton 2018")
-
 # https://github.com/ewenme/understatr/blob/master/R/teams.R
+# Get information on a team's available seasons
 team_meta <- function(team_name) {
   
-  team_name <- str_replace_all(team_name, " ", "_")
+  # team_name <- str_replace_all(team_name, " ", "_") #old
   
-  team_url <- str_glue("https://understat.com/team/{team_name}")
+  # team_url <- str_glue("https://understat.com/team/{team_name}") #old
+  team_url <- paste0("https://understat.com/team/",team_name) %>%
+    str_replace_all(" ", "_")
   
   # read understat team page
-  team_page <- read_html(team_url)
+  # team_page <- read_html(team_url)
   
   # isolate league links
-  year_link <- html_nodes(team_page, "#header :nth-child(2)")
+  year_link <- read_html(team_url) %>%
+    html_nodes("#header :nth-child(2)")
   
   # isolate year options
   year_options <- html_nodes(year_link[2], "option")
   
   # create league fields as df
-  team_df <- data.frame(
+  team_meta <- data.frame(
     team_name = team_name,
     year = as.numeric(html_attr(year_options, "value")),
     season = html_text(year_options),
     stringsAsFactors = FALSE
   )
   
+  team_meta %<>%
+    mutate(url=file.path(team_url, year)) %>%
+    as_tibble
   # create url per season
-  team_df$url <- file.path(team_url, team_df$year)
+  # team_df$url <- file.path(team_url, team_df$year)
   
-  return(team_df)
-  
+  return(team_meta)
 }
 
 team_players_stats <- function(team_name, year) {
