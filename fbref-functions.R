7b511b2751a1998eb3a626877631f8eae6dc9ff0
saintsnumbers <52595717+saintsnumbers@users.noreply.github.com>
2019-11-15 17:05:00 +0000

    add rvest code

---
+++ fbref-functions.R
@@ -0,0 +1,9 @@
+url_fbref <- "https://fbref.com/en/"
+url_epl_1920 <- "https://fbref.com/en/comps/9/Premier-League-Stats"
+
+# scraped_page <- read_html(url_epl_1920)
+
+# scraped_page %>%
+#   html_nodes("td") %>%
+#   html_text %>%
+#   View()
