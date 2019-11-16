ab8843e031169b5a854ebaa2808f35d8c5471ac7
saintsnumbers <52595717+saintsnumbers@users.noreply.github.com>
2019-11-16 16:21:55 +0000

    add comment symbol

--- fbref-import.R
+++ fbref-import.R
@@ -1,5 +1,5 @@
-EPLTable_raw <- readr::read_csv("./data/fbref/2019/EPLTable.txt") # https://fbref.com/en/comps/9/Premier-League-Stats#results32321::none
-EPLTeamStats_raw <- readr::read_csv("./data/fbref/2019/EPLTeamStats.txt") # https://fbref.com/en/comps/9/stats/Premier-League-Stats#stats_player_teams::none
-EPLPlayerStats_raw <- readr::read_csv("./data/fbref/2019/EPLPlayerStats.txt") # https://fbref.com/en/comps/9/stats/Premier-League-Stats#stats_player::none
-SFCMatches_raw <- readr::read_csv("./data/fbref/2019/SFCMatches.txt") # https://fbref.com/en/squads/33c895d4/Southampton#ks_sched_all::none
-SFCPlayerStats_raw <- readr::read_csv("./data/fbref/2019/SFCPlayerStats.txt") # https://fbref.com/en/squads/33c895d4/Southampton#stats_player::none
+EPLTable_raw <- readr::read_csv("./data/fbref/2019/EPLTable.txt",comment="##") # https://fbref.com/en/comps/9/Premier-League-Stats#results32321::none
+EPLTeamStats_raw <- readr::read_csv("./data/fbref/2019/EPLTeamStats.txt",comment="##",skip=1) # https://fbref.com/en/comps/9/stats/Premier-League-Stats#stats_player_teams::none
+EPLPlayerStats_raw <- readr::read_csv("./data/fbref/2019/EPLPlayerStats.txt",comment="##") # https://fbref.com/en/comps/9/stats/Premier-League-Stats#stats_player::none
+SFCMatches_raw <- readr::read_csv("./data/fbref/2019/SFCMatches.txt",comment="##") # https://fbref.com/en/squads/33c895d4/Southampton#ks_sched_all::none
+SFCPlayerStats_raw <- readr::read_csv("./data/fbref/2019/SFCPlayerStats.txt",comment="##") # https://fbref.com/en/squads/33c895d4/Southampton#stats_player::none
