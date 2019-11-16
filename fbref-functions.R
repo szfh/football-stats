2d96fb78e51f9f6e1824772c656418a653424c37
saintsnumbers <52595717+saintsnumbers@users.noreply.github.com>
2019-11-16 14:29:27 +0000

    move data import to correct file

--- fbref-functions.R
+++ fbref-functions.R
@@ -7,9 +7,3 @@ url_epl_1920 <- "https://fbref.com/en/comps/9/Premier-League-Stats"
 #   html_nodes("td") %>%
 #   html_text %>%
 #   View()
-
-EPLTable <- readr::read_csv("./data/fbref/2019/EPLTable.txt") # https://fbref.com/en/comps/9/Premier-League-Stats#results32321::none
-EPLTeamStats <- readr::read_csv("./data/fbref/2019/EPLTeamStats.txt") # https://fbref.com/en/comps/9/stats/Premier-League-Stats#stats_player_teams::none
-EPLPlayerStats <- readr::read_csv("./data/fbref/2019/EPLPlayerStats.txt") # https://fbref.com/en/comps/9/stats/Premier-League-Stats#stats_player::none
-SFCMatches <- readr::read_csv("./data/fbref/2019/SFCMatches.txt") # https://fbref.com/en/squads/33c895d4/Southampton#ks_sched_all::none
-SFCPlayerStats <- readr::read_csv("./data/fbref/2019/SFCPlayerStats.txt") # https://fbref.com/en/squads/33c895d4/Southampton#stats_player::none
