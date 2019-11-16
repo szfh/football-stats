46ec4cb816ac8f7798b64d50a124ef64ba4d42ed
saintsnumbers <52595717+saintsnumbers@users.noreply.github.com>
2019-11-16 19:24:30 +0000

    tidy EPL Matches

--- fbref-import.R
+++ fbref-import.R
@@ -1,6 +1,7 @@
 EPLTable_raw <- readr::read_csv("./data/fbref/2019/EPLTable.txt",comment="##") # https://fbref.com/en/comps/9/Premier-League-Stats#results32321::none
 EPLTeamStats_raw <- readr::read_csv("./data/fbref/2019/EPLTeamStats.txt",comment="##",skip=1) # https://fbref.com/en/comps/9/stats/Premier-League-Stats#stats_player_teams::none
 EPLPlayerStats_raw <- readr::read_csv("./data/fbref/2019/EPLPlayerStats.txt",comment="##",skip=1) # https://fbref.com/en/comps/9/stats/Premier-League-Stats#stats_player::none
+EPLMatches_raw <-readr::read_csv("./data/fbref/2019/EPLMatches.txt",comment="##") # https://fbref.com/en/comps/9/schedule/Premier-League-Fixtures
 SFCMatches_raw <- readr::read_csv("./data/fbref/2019/SFCMatches.txt",comment="##") # https://fbref.com/en/squads/33c895d4/Southampton#ks_sched_all::none
 # SFCPlayerStats_raw <- readr::read_csv("./data/fbref/2019/SFCPlayerStats.txt",comment="##",skip=1) # https://fbref.com/en/squads/33c895d4/Southampton#stats_player::none
 
@@ -39,3 +40,11 @@ EPLPlayerStats <- EPLPlayerStats_raw %>%
   rename("RC"="CrdR") %>%
   select(-c("Rk","Mn/Ap","Gls_1":"npxG+xA")) %>%
   View("EPLPlayerStats")
+
+EPLMatches <- EPLMatches_raw %>%
+  separate("Score",c("GH","GA"),sep="[:punct:]") %>%
+  rename("xGH"="xG") %>%
+  rename("xGA"="xG_1") %>%
+  filter(!is.na(Wk)) %>%
+  select(-c("Match Report","Notes")) %>%
+  View("EPLMatches")
