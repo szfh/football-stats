fbc952074cf098f1ab5e1ec9498d2839ec1a624a
saintsnumbers <52595717+saintsnumbers@users.noreply.github.com>
2019-11-16 16:22:49 +0000

    tidy EPLTable

--- fbref-import.R
+++ fbref-import.R
@@ -3,3 +3,11 @@ EPLTeamStats_raw <- readr::read_csv("./data/fbref/2019/EPLTeamStats.txt",comment
 EPLPlayerStats_raw <- readr::read_csv("./data/fbref/2019/EPLPlayerStats.txt",comment="##") # https://fbref.com/en/comps/9/stats/Premier-League-Stats#stats_player::none
 SFCMatches_raw <- readr::read_csv("./data/fbref/2019/SFCMatches.txt",comment="##") # https://fbref.com/en/squads/33c895d4/Southampton#ks_sched_all::none
 SFCPlayerStats_raw <- readr::read_csv("./data/fbref/2019/SFCPlayerStats.txt",comment="##") # https://fbref.com/en/squads/33c895d4/Southampton#stats_player::none
+
+EPLTable <- EPLTable_raw %>%
+  rename("Pos"="Rk") %>%
+  rename("P"="Apps") %>%
+  rename("GD"="GDiff") %>%
+  rename("xGD"="xGDiff") %>%
+  select(-c("Top Team Scorer",Goalkeeper)) %>%
+  View("EPLTable")
