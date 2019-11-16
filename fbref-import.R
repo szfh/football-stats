bdfe7defa143729af8e66bf6edd81e97521805ea
saintsnumbers <52595717+saintsnumbers@users.noreply.github.com>
2019-11-16 17:52:17 +0000

    remove SFCPlayerStats

--- fbref-import.R
+++ fbref-import.R
@@ -2,7 +2,7 @@ EPLTable_raw <- readr::read_csv("./data/fbref/2019/EPLTable.txt",comment="##") #
 EPLTeamStats_raw <- readr::read_csv("./data/fbref/2019/EPLTeamStats.txt",comment="##",skip=1) # https://fbref.com/en/comps/9/stats/Premier-League-Stats#stats_player_teams::none
 EPLPlayerStats_raw <- readr::read_csv("./data/fbref/2019/EPLPlayerStats.txt",comment="##",skip=1) # https://fbref.com/en/comps/9/stats/Premier-League-Stats#stats_player::none
 SFCMatches_raw <- readr::read_csv("./data/fbref/2019/SFCMatches.txt",comment="##") # https://fbref.com/en/squads/33c895d4/Southampton#ks_sched_all::none
-SFCPlayerStats_raw <- readr::read_csv("./data/fbref/2019/SFCPlayerStats.txt",comment="##") # https://fbref.com/en/squads/33c895d4/Southampton#stats_player::none
+# SFCPlayerStats_raw <- readr::read_csv("./data/fbref/2019/SFCPlayerStats.txt",comment="##",skip=1) # https://fbref.com/en/squads/33c895d4/Southampton#stats_player::none
 
 EPLTable <- EPLTable_raw %>%
   rename("Pos"="Rk") %>%
