5413ec58c3245a72bbccfdf6d9876bdbeeffebfa
saintsnumbers <52595717+saintsnumbers@users.noreply.github.com>
2019-11-20 17:54:22 +0000

    rearrange tidy

--- R/fbref-tidy.R
+++ R/fbref-tidy.R
@@ -1,21 +1,17 @@
-EPLTable <- EPLTable_raw %>%
-  rename("Pos"="Rk") %>%
-  rename("P"="Apps") %>%
-  rename("GD"="GDiff") %>%
-  rename("xGD"="xGDiff") %>%
-  select(-c("Top Team Scorer",Goalkeeper))
+EPLMatches <- EPLMatches_raw %>%
+  separate("Score",c("GH","GA"),sep="[:punct:]") %>%
+  rename("xGH"="xG") %>%
+  rename("xGA"="xG_1") %>%
+  filter(!is.na(Wk)) %>%
+  select(-c("Match Report","Notes"))
 
-EPLTeamStats <- EPLTeamStats_raw %>%
-  rename("Players"="# Pl") %>%
-  rename("Played"="Apps") %>%
+EPLPlayerShooting <- EPLPlayerShooting_raw %>%
+  separate("Player",c("Player",NA),sep="\\\\") %>%
+  separate("Nation",c(NA,"Nation"),sep=" ") %>%
   rename("Goals"="Gls") %>%
-  rename("Assists"="Ast") %>%
   rename("PKGoals"="PK") %>%
   rename("PKs"="PKatt") %>%
-  rename("Fouls"="Fls") %>%
-  rename("YC"="CrdY") %>%
-  rename("RC"="CrdR") %>%
-  select(-("Gls_1":"Crd"))
+  View("PlayerShooting")
 
 EPLPlayerStats <- EPLPlayerStats_raw %>%
   separate("Player",c("Player",NA),sep="\\\\") %>%
@@ -31,15 +27,12 @@ EPLPlayerStats <- EPLPlayerStats_raw %>%
   rename("RC"="CrdR") %>%
   select(-c("Rk","Mn/Ap","Gls_1":"npxG+xA"))
 
-EPLMatches <- EPLMatches_raw %>%
-  separate("Score",c("GH","GA"),sep="[:punct:]") %>%
-  rename("xGH"="xG") %>%
-  rename("xGA"="xG_1") %>%
-  filter(!is.na(Wk)) %>%
-  select(-c("Match Report","Notes"))
-
-SFCMatches <- SFCMatches_raw %>%
-  select(-c("Match Report","Notes"))
+EPLTable <- EPLTable_raw %>%
+  rename("Pos"="Rk") %>%
+  rename("P"="Apps") %>%
+  rename("GD"="GDiff") %>%
+  rename("xGD"="xGDiff") %>%
+  select(-c("Top Team Scorer",Goalkeeper))
 
 EPLTeamShooting <- EPLTeamShooting_raw %>%
   rename("Players"="# Pl") %>%
@@ -49,10 +42,17 @@ EPLTeamShooting <- EPLTeamShooting_raw %>%
   rename("Shots"="Sh") %>%
   select(-"SoT%")
 
-EPLPlayerShooting <- EPLPlayerShooting_raw %>%
-  separate("Player",c("Player",NA),sep="\\\\") %>%
-  separate("Nation",c(NA,"Nation"),sep=" ") %>%
+EPLTeamStats <- EPLTeamStats_raw %>%
+  rename("Players"="# Pl") %>%
+  rename("Played"="Apps") %>%
   rename("Goals"="Gls") %>%
+  rename("Assists"="Ast") %>%
   rename("PKGoals"="PK") %>%
   rename("PKs"="PKatt") %>%
-  View("PlayerShooting")
+  rename("Fouls"="Fls") %>%
+  rename("YC"="CrdY") %>%
+  rename("RC"="CrdR") %>%
+  select(-("Gls_1":"Crd"))
+
+SFCMatches <- SFCMatches_raw %>%
+  select(-c("Match Report","Notes"))
