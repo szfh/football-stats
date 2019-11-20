2e246572277efb79dc9e78f2249a9dcbc902918b
saintsnumbers <52595717+saintsnumbers@users.noreply.github.com>
2019-11-20 17:10:04 +0000

    separate and tidy

--- R/fbref-import.R
+++ R/fbref-import.R
@@ -1,9 +1,25 @@
 # import from ./data/fbref/2019/
-EPLMatches_raw <- readr::read_csv("./data/fbref/2019/EPLMatches.txt",comment="##") # https://fbref.com/en/comps/9/schedule/Premier-League-Fixtures
-# EPLPassing_raw <- readr::read_csv("./data/fbref/2019/EPLPassing.txt",comment="##") # 
-EPLPlayerShooting_raw <- readr::read_csv("./data/fbref/2019/EPLPlayerShooting.txt",comment="##") #
-EPLPlayerStats_raw <- readr::read_csv("./data/fbref/2019/EPLPlayerStats.txt",comment="##",skip=1) # https://fbref.com/en/comps/9/stats/Premier-League-Stats
-EPLTable_raw <- readr::read_csv("./data/fbref/2019/EPLTable.txt",comment="##") # https://fbref.com/en/comps/9/Premier-League-Stats
-EPLTeamShooting_raw <- readr::read_csv("./data/fbref/2019/EPLTeamShooting.txt",comment="##") #
-EPLTeamStats_raw <- readr::read_csv("./data/fbref/2019/EPLTeamStats.txt",comment="##",skip=1) # https://fbref.com/en/comps/9/stats/Premier-League-Stats
-SFCMatches_raw <- readr::read_csv("./data/fbref/2019/SFCMatches.txt",comment="##") # https://fbref.com/en/squads/33c895d4/Southampton
+
+EPLMatches_raw <- readr::read_csv("./data/fbref/2019/EPLMatches.txt",skip=1,comment="##")
+# https://fbref.com/en/comps/9/schedule/Premier-League-Fixtures
+
+# EPLPassing_raw <- readr::read_csv("./data/fbref/2019/EPLPassing.txt",comment="##")
+# https://fbref.com/en/comps/9/passing/Premier-League-Stats
+
+EPLPlayerShooting_raw <- readr::read_csv("./data/fbref/2019/EPLPlayerShooting.txt",skip=1,comment="##")
+# https://fbref.com/en/comps/9/shooting/Premier-League-Stats
+
+EPLPlayerStats_raw <- readr::read_csv("./data/fbref/2019/EPLPlayerStats.txt",comment="##",skip=2)
+# https://fbref.com/en/comps/9/stats/Premier-League-Stats
+
+EPLTable_raw <- readr::read_csv("./data/fbref/2019/EPLTable.txt",skip=1,comment="##")
+# https://fbref.com/en/comps/9/Premier-League-Stats
+
+EPLTeamShooting_raw <- readr::read_csv("./data/fbref/2019/EPLTeamShooting.txt",comment="##")
+# https://fbref.com/en/comps/9/shooting/Premier-League-Stats
+
+EPLTeamStats_raw <- readr::read_csv("./data/fbref/2019/EPLTeamStats.txt",comment="##",skip=2)
+# https://fbref.com/en/comps/9/stats/Premier-League-Stats
+
+SFCMatches_raw <- readr::read_csv("./data/fbref/2019/SFCMatches.txt",comment="##")
+# https://fbref.com/en/squads/33c895d4/Southampton
