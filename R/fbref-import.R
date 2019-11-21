816f37107f60e4dc881f60efcce7bd39942b8f52
saintsnumbers <52595717+saintsnumbers@users.noreply.github.com>
2019-11-21 17:22:41 +0000

    import passing

--- R/fbref-import.R
+++ R/fbref-import.R
@@ -3,7 +3,7 @@
 EPLMatches_raw <- readr::read_csv("./data/fbref/2019/EPLMatches.txt",skip=1,comment="##")
 # https://fbref.com/en/comps/9/schedule/Premier-League-Fixtures
 
-# EPLPassing_raw <- readr::read_csv("./data/fbref/2019/EPLPassing.txt",comment="##")
+EPLPlayerPassing_raw <- readr::read_csv("./data/fbref/2019/EPLPlayerPassing.txt",skip=2,comment="##")
 # https://fbref.com/en/comps/9/passing/Premier-League-Stats
 
 EPLPlayerShooting_raw <- readr::read_csv("./data/fbref/2019/EPLPlayerShooting.txt",skip=1,comment="##")
@@ -15,10 +15,13 @@ EPLPlayerStats_raw <- readr::read_csv("./data/fbref/2019/EPLPlayerStats.txt",com
 EPLTable_raw <- readr::read_csv("./data/fbref/2019/EPLTable.txt",skip=1,comment="##")
 # https://fbref.com/en/comps/9/Premier-League-Stats
 
+EPLTeamPassing_raw <- readr::read_csv("./data/fbref/2019/EPLTeamPassing.txt",skip=2,comment="##")
+# https://fbref.com/en/comps/9/passing/Premier-League-Stats
+
 EPLTeamShooting_raw <- readr::read_csv("./data/fbref/2019/EPLTeamShooting.txt",comment="##")
 # https://fbref.com/en/comps/9/shooting/Premier-League-Stats
 
-EPLTeamStats_raw <- readr::read_csv("./data/fbref/2019/EPLTeamStats.txt",comment="##",skip=2)
+EPLTeamStats_raw <- readr::read_csv("./data/fbref/2019/EPLTeamStats.txt",skip=2,comment="##")
 # https://fbref.com/en/comps/9/stats/Premier-League-Stats
 
 SFCMatches_raw <- readr::read_csv("./data/fbref/2019/SFCMatches.txt",comment="##")
