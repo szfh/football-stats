6fae2fd533a7f2fee0dcc14b8d68b65d079f7848
saintsnumbers <52595717+saintsnumbers@users.noreply.github.com>
2019-12-06 18:18:42 +0000

    table scraper

--- R/fbref-scraper.R
+++ R/fbref-scraper.R
@@ -1,19 +1,18 @@
-# htmlbb <- "https://www.basketball-reference.com/players/j/jamesle01.html" %>% read_html()
-
-# htmlsfc <- "https://fbref.com/en/squads/33c895d4/Southampton" %>% read_html()
-
 # htmlepl <- "https://fbref.com/en/comps/9/Premier-League-Stats" %>% read_html
 
-htmlbb %>%
-  html_nodes(".full_table .right , .full_table .center , #per_game .full_table .left") %>% 
-  html_text %>% 
-  matrix(ncol = 30, byrow = T) %>% 
-  as.data.frame %>%
-  View()
-
-tableepl <- htmlepl %>%
+tableepl_tibble <-
+  htmlepl %>%
   html_nodes("#results32321_overall .center , #results32321_overall .right, #results32321_overall .left, #results32321_overall .right") %>%
   html_text %>%
   matrix(nrow = 21, byrow = T) %>%
-  as.data.frame()
-# as_tibble
+  as_tibble()
+
+tableepl_raw <- tableepl_tibble %>%
+  set_colnames(tableepl_tibble[1,]) %>%
+  slice(-1)
+
+tableepl_tibble %>% View("tibble")
+tableepl_raw %>% View("raw")
+# rm(tableepl_tibble,tableepl_raw)
+
+write.csv(tableepl_raw,file="data/fbref/tableepl_raw.csv",row.names=F)
