b204c8fdd84c9e6279118198daca2fdd4505003a
saintsnumbers <52595717+saintsnumbers@users.noreply.github.com>
2019-12-06 18:35:57 +0000

    start pipeline with url

--- R/fbref-scraper.R
+++ R/fbref-scraper.R
@@ -1,18 +1,19 @@
-# htmlepl <- "https://fbref.com/en/comps/9/Premier-League-Stats" %>% read_html
+table_url <- "https://fbref.com/en/comps/9/Premier-League-Stats"
 
-tableepl_tibble <-
-  htmlepl %>%
+table_tibble <-
+  table_url %>%
+  read_html() %>%
   html_nodes("#results32321_overall .center , #results32321_overall .right, #results32321_overall .left, #results32321_overall .right") %>%
   html_text %>%
   matrix(nrow = 21, byrow = T) %>%
   as_tibble()
 
-tableepl_raw <- tableepl_tibble %>%
+table_raw <- table_tibble %>%
   set_colnames(tableepl_tibble[1,]) %>%
   slice(-1)
 
-tableepl_tibble %>% View("tibble")
-tableepl_raw %>% View("raw")
+table_tibble %>% View("tibble")
+table_raw %>% View("raw")
 # rm(tableepl_tibble,tableepl_raw)
 
-write.csv(tableepl_raw,file="data/fbref/tableepl_raw.csv",row.names=F)
+write.csv(tableepl_raw,file="data/fbref/table_raw.csv",row.names=F)
