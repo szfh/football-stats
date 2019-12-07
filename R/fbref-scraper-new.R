5322dbc2c41610bb679b324eb8445ed506a88f4b
saintsnumbers <52595717+saintsnumbers@users.noreply.github.com>
2019-12-07 16:33:22 +0000

    scrape squad standard and shooting tables

--- R/fbref-scraper-new.R
+++ R/fbref-scraper-new.R
@@ -1,18 +1,26 @@
+source("./R/fbref-scraper-functions.R")
 # table
-table_raw <- fbref_scrape(url="https://fbref.com/en/comps/9/Premier-League-Stats",ncol=19,fix_columns=TRUE,
-                            nodes="#results32321_overall .center , #results32321_overall .right, #results32321_overall .left, #results32321_overall .right")
+table_raw <- fbref_scrape(ncol=19,fix_columns=TRUE,
+                          url="https://fbref.com/en/comps/9/Premier-League-Stats",
+                          nodes="#results32321_overall .center , #results32321_overall .right, #results32321_overall .left, #results32321_overall .right")
 
 # matches
-matches_raw <- fbref_scrape(url="https://fbref.com/en/comps/9/schedule/Premier-League-Fixtures",ncol=14,fix_columns=TRUE,
+matches_raw <- fbref_scrape(ncol=14,fix_columns=TRUE,remove_last_row=TRUE,
+                            url="https://fbref.com/en/comps/9/schedule/Premier-League-Fixtures",
                             nodes=".left , .right, .center")
 
 # squad stats
-standard_squad <- fbref_scrape(url="https://fbref.com/en/comps/9/stats/Premier-League-Stats",ncol=1,
-             nodes="#stats_standard_squads .left , #stats_standard_squads .right, #stats_standard_squads .center")
+squad_standard_raw <- fbref_scrape(ncol=24,skip_head=6,fix_columns=TRUE,
+                               url="https://fbref.com/en/comps/9/stats/Premier-League-Stats",
+                               nodes="#stats_standard_squads .left , #stats_standard_squads .right, #stats_standard_squads .center")
 
+squad_shooting_raw <- fbref_scrape(ncol=18,skip_head=0,fix_columns=TRUE,
+                                   url="https://fbref.com/en/comps/9/shooting/Premier-League-Stats",
+                                   nodes="#stats_shooting_squads .left , #stats_shooting_squads .right, #stats_shooting_squads .center")
 
-# fbref_scrape(url="",ncol=,fix_columns=FALSE,
-#              nodes="")
+# fbref_scrape(ncol=1,skip_head=0,fix_columns=F,
+#                                    url="https://fbref.com/en/comps/9/stats/Premier-League-Stats",
+#                                    nodes="")
 
 # data <-
 #   url %>%
