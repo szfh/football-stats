6f38e50e6392bff9b2dc0d5104f81fea470e1dc8
saintsnumbers <52595717+saintsnumbers@users.noreply.github.com>
2019-12-07 10:05:48 +0000

    scrape table

--- R/fbref-scraper-new.R
+++ R/fbref-scraper-new.R
@@ -1,34 +1,5 @@
-# table
-table_url <- "https://fbref.com/en/comps/9/Premier-League-Stats"
-
-table_tibble <-
-  table_url %>%
-  read_html() %>%
-  html_nodes("#results32321_overall .center , #results32321_overall .right, #results32321_overall .left, #results32321_overall .right") %>%
-  html_text %>%
-  matrix(nrow = 21, byrow = T) %>%
-  as_tibble()
-
-table_raw <- table_tibble %>%
-  set_colnames(tableepl_tibble[1,]) %>%
-  slice(-1)
-
-table_tibble %>% View("tibble")
-table_raw %>% View("raw")
-# rm(tableepl_tibble,tableepl_raw)
-
-write.csv(table_raw,file="data/fbref/table_raw.csv",row.names=F)
-
-# matches
-matches_url <- "https://fbref.com/en/comps/9/schedule/Premier-League-Fixtures"
-
-matches_tibble <-
-  matches_url %>%
-  read_html() %>%
-  html_nodes(".left , .right, .center") %>%
-  html_text %>%
-  matrix(ncol = 14, byrow = T)
-
-write.csv(matches_raw,file="data/fbref/matches_raw.csv",row.names=F)
+# write.csv(matches_raw,file="data/fbref/matches_raw.csv",row.names=F)
 
 
+table_raw <- fbref_scrape(url="https://fbref.com/en/comps/9/Premier-League-Stats",ncol=19,fix_columns=TRUE,
+                            nodes="#results32321_overall .center , #results32321_overall .right, #results32321_overall .left, #results32321_overall .right")
