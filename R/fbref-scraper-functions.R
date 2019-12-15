ebd1e3841405b1465eeba6ff8a51b482462d37db
saintsnumbers <52595717+saintsnumbers@users.noreply.github.com>
2019-12-15 10:07:35 +0000

    read data manually using script

--- R/fbref-scraper-functions.R
+++ R/fbref-scraper-functions.R
@@ -70,16 +70,16 @@ read_data <- function(){
   #               list.files(path="./data",pattern="_raw.rds"))
   # )
   
-  filepath <- "./data/"
-  
-  matches_raw <- readRDS(paste0(filepath,"matches_raw.rds"))
-  squad_keepers_raw <- readRDS(paste0(filepath,"squad_keepers_raw.rds"))
-  squad_misc_raw <- readRDS(paste0(filepath,"squad_misc_raw.rds"))
-  squad_passing_raw <- readRDS(paste0(filepath,"squad_passing_raw.rds"))
-  squad_playingtime_raw <- readRDS(paste0(filepath,"squad_playingtime_raw.rds"))
-  squad_shooting_raw <- readRDS(paste0(filepath,"squad_shooting_raw.rds"))
-  squad_standard_raw <- readRDS(paste0(filepath,"squad_standard_raw.rds"))
-  table_raw <- readRDS(paste0(filepath,"table_raw.rds"))
+  # filepath <- "./data/"
+
+  # matches_raw <- readRDS(paste0(filepath,"matches_raw.rds"))
+  # squad_keepers_raw <- readRDS(paste0(filepath,"squad_keepers_raw.rds"))
+  # squad_misc_raw <- readRDS(paste0(filepath,"squad_misc_raw.rds"))
+  # squad_passing_raw <- readRDS(paste0(filepath,"squad_passing_raw.rds"))
+  # squad_playingtime_raw <- readRDS(paste0(filepath,"squad_playingtime_raw.rds"))
+  # squad_shooting_raw <- readRDS(paste0(filepath,"squad_shooting_raw.rds"))
+  # squad_standard_raw <- readRDS(paste0(filepath,"squad_standard_raw.rds"))
+  # table_raw <- readRDS(paste0(filepath,"table_raw.rds"))
 }
 
 # readRDS(paste0("./data/","matches_raw.rds"))
