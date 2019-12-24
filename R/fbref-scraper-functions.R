bb7a0a1a05b70e6ff3fcf4810ba05e748a9bbfa0
saintsnumbers <52595717+saintsnumbers@users.noreply.github.com>
2019-12-24 19:23:28 +0000

    remove commented old function

--- R/fbref-scraper-functions.R
+++ R/fbref-scraper-functions.R
@@ -23,63 +23,6 @@ fbref_scrape <- function(fbref_url,nodes=NA,fix_columns=FALSE,extract=NA){
   return(data_table)
 }
 
-# function to scrape fbref
-# fbref_scrape <- function(fbref_url,nodes,ncol=1,skip_head=0,skip_tail=0,fix_columns=FALSE,remove_last_row=FALSE){
-# # browser()
-#   data_text <-
-#     fbref_url %>%
-#     read_html() %>%
-#     html_nodes(nodes) %>%
-#     html_text
-# 
-#   if(skip_head!=0){
-#     data_text <- data_text %>%
-#       enframe() %>%
-#       slice(-c(1:skip_head)) %>%
-#       deframe
-#   }
-#   
-#   if(skip_tail!=0){
-#     data_text <- data_text %>%
-#       enframe() %>%
-#       slice(-c(length(data_text)-skip_tail+1:length(data_text))) %>%
-#       deframe
-#   }
-#   
-#   data <- data_text %>%
-#     matrix(ncol=ncol,byrow=T) %>%
-#     as_tibble(.name_repair = "minimal")
-# 
-#   if(fix_columns==TRUE){
-#     data <- first_row_to_columns(data)
-#   }
-#   
-#   if(remove_last_row==TRUE){
-#     data <- remove_last_row(data)
-#   }
-# 
-#   return(data)
-# }
-
-#function to make the first row of a tibble into unique column names, then delete the first row
-# first_row_to_columns <- function(data){
-#   data <- data %>%
-#     set_colnames(
-#       make.names(data[1,],unique=TRUE)
-#       ) %>%
-#     slice(-1)
-# 
-#   return(data)
-# }
-
-#function to remove the last row containing extra data
-# remove_last_row <- function(data){
-#   data <- data %>%
-#     slice(-nrow(data))
-#   
-#   return(data)
-# }
-
 #function to save data as RDS
 save_data <- function(data){
   saveRDS(
