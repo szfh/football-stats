3e89056e15c3c8001edc72ba2b81aac01a8db245
saintsnumbers <52595717+saintsnumbers@users.noreply.github.com>
2019-12-30 19:22:22 +0000

    tidy matches

--- R/tidy/fbref-tidy-matches.R
+++ R/tidy/fbref-tidy-matches.R
@@ -1,11 +1,16 @@
-Matches <- Matches_raw %>%
-  rename(
-    "xGHome"="xG",
-    "xGAway"="xG_1",
-  ) %>%
+tidy[["matches"]] <- raw[["matches"]] %>%
   separate("Score",c("GoalsHome","GoalsAway"),sep="[:punct:]") %>%
+  rename(
+    "xGHome"="xG...6",
+    "xGAway"="xG...8",
+  ) %>%
+  mutate(
+    GoalsHome=as.numeric(GoalsHome),
+    GoalsAway=as.numeric(GoalsAway),
+    Attendance=as.numeric(gsub("\\,","",Attendance)),
+      ) %>%
   select(
     -"Match Report",
     -"Notes",
   ) %>%
-  drop_na()
+  drop_na("Wk")
