d7016fab68e4005f3b2c62bc575e63cc937b3d51
saintsnumbers <52595717+saintsnumbers@users.noreply.github.com>
2020-01-06 20:35:07 +0000

    create fbref-scraper.py

---
+++ python/fbref-scraper.py
@@ -0,0 +1,8 @@
+# https://github.com/chmartin/FBref_EPL/blob/master/FBref_scrape.py
+
+# import requests
+# from bs4 import BeautifulSoup
+import pandas as pd
+# import re
+# import sys, getopt
+# import csv
