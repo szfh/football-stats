a5832f9ddd588865d76f6c58c4bd3239d71b5003
saintsnumbers <52595717+saintsnumbers@users.noreply.github.com>
2020-01-07 17:45:12 +0000

    remove python from branch

--- python/fbref-scraper.py
+++
@@ -1,8 +0,0 @@
-# https://github.com/chmartin/FBref_EPL/blob/master/FBref_scrape.py
-
-# import requests
-# from bs4 import BeautifulSoup
-import pandas as pd
-# import re
-# import sys, getopt
-# import csv
