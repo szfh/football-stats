124dc2d4845b06884abb839fde1438511b350d00
saintsnumbers <52595717+saintsnumbers@users.noreply.github.com>
2020-01-09 16:27:32 +0000

    tidy scraped player data

--- R/fbref/tidy/fbref-tidy-players.R
+++ R/fbref/tidy/fbref-tidy-players.R
@@ -1,72 +1,79 @@
 tidy[["player"]][["standard"]] <- raw[["player"]][["standard"]] %>%
   rename(
-    "Gls90"="Gls_1",
-    "Ast90"="Ast_1",
+    "Gls"="Gls...11",
+    "Ast"="Ast...12",
+    "Gls90"="Gls...17",
+    "Ast90"="Ast...18",
     "G+A90"="G+A",
     "G-PK90"="G-PK",
     "G+A-PK90"="G+A-PK",
-    "xG90"="xG_1",
-    "xA90"="xA_1",
+    "xG"="xG...22",
+    "npxG"="npxG...23",
+    "xA"="xA...24",
+    "xG90"="xG...25",
+    "xA90"="xA...26",
     "xG+xA90"="xG+xA",
-    "npxG90"="npxG_1",
+    "npxG90"="npxG...28",
     "npxG+xA90"="npxG+xA",
+  ) %>%
+  select(
+    -"Rk",
   )
 
 tidy[["player"]][["passing"]] <- raw[["player"]][["passing"]] %>%
   rename(
-    "TotalCmp"="Cmp",
-    "TotalAtt"="Att",
-    "TotalCmp%"="Cmp%",
-    "ShortCmp"="Cmp_1",
-    "ShortAtt"="Att_1",
-    "ShortCmp%"="Cmp%_1",
-    "MediumCmp"="Cmp_2",
-    "MediumAtt"="Att_2",
-    "MediumCmp%"="Cmp%_2",
-    "LongCmp"="Cmp_3",
-    "LongAtt"="Att_3",
-    "LongCmp%"="Cmp%_3",
+    "TotalCmp"="Cmp...11",
+    "TotalAtt"="Att...12",
+    "TotalCmp%"="Cmp%...13",
+    "ShortCmp"="Cmp...14",
+    "ShortAtt"="Att...15",
+    "ShortCmp%"="Cmp%...16",
+    "MediumCmp"="Cmp...17",
+    "MediumAtt"="Att...18",
+    "MediumCmp%"="Cmp%...19",
+    "LongCmp"="Cmp...20",
+    "LongAtt"="Att...21",
+    "LongCmp%"="Cmp%...22",
   ) %>%
   select(
     -"Rk",
-    -"Ast",
-    -"xA",
   )
 
 tidy[["player"]][["shooting"]] <- raw[["player"]][["shooting"]] %>%
+  rename(
+    "Sh90"="Sh/90",
+    "SoT90"="SoT/90",
+  ) %>%
   select(
     -"Rk",
-    -"Gls",
-    -"PK",
-    -"PKatt",
-    -"xG",
-    -"npxG",
-    -"90s",
-    -"FK",
   )
 
 tidy[["player"]][["misc"]] <- raw[["player"]][["misc"]] %>%
+  rename(
+    "DribSuc"="Succ",
+    "DribAtt"="Att...20",
+    "Drib#Pl"="#Pl",
+    "DribTkl"="Tkl",
+    "DribCont"="Att...25",
+    "DribTkl%"="Tkl%",
+    "DribPast"="Past",
+  ) %>%
   select(
     -"Rk",
-    -"CrdY",
-    -"CrdR",
-    -"90s",
   )
 
 tidy[["player"]][["playingtime"]] <- raw[["player"]][["playingtime"]] %>%
   rename(
-    "xGOn-Off"="On-Off_1"
+    "Gls+/-"="+/-",
+    "Gls+/-90"="+/-90",
+    "GlsOn-Off"="On-Off...21",
+    "xGOn-Off"="On-Off...26",
   ) %>%
   select(
     -"Rk",
-    -"MP",
-    -"Starts",
-    -"Min",
-    -"90s",
-    -"Matches",
   )
 
 players <- reduce(tidy[["player"]],left_join) %>%
-  separate("Player",c("Player",NA),sep="\\\\") %>%
-  separate("Nation",c(NA,"Nation"),sep=" ") %>%
-  separate("Pos",c("Pos1","Pos2"),sep=2)
+  separate("Player",c("Player",NA),sep="\\\\",fill="right") %>%
+  separate("Nation",c(NA,"Nation"),sep=" ",fill="right") %>%
+  separate("Pos",c("Pos1",NA,"Pos2"),sep=c(2,3),fill="right")
