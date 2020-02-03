fc5d6916dc943b1e83673ba9ed68b6d22512cb09
saintsnumbers <52595717+saintsnumbers@users.noreply.github.com>
2020-02-03 21:48:09 +0000

    scrape fivethirtyeight club rankings

--- fivethirtyeight-scraper.R
+++ fivethirtyeight-scraper.R
@@ -1,13 +1,18 @@
 library(tidyverse)
 
-fivethirtyeight <- read.csv("https://projects.fivethirtyeight.com/soccer-api/club/spi_matches.csv")
+spi_matches <- read.csv("https://projects.fivethirtyeight.com/soccer-api/club/spi_matches.csv")
+spi_rankings <- read.csv("https://projects.fivethirtyeight.com/soccer-api/club/spi_global_rankings.csv")
 
-fte_leagues <- fivethirtyeight %>%
+spi_leagues <- spi_matches %>%
   select(league,league_id) %>%
   unique()
 
-fivethirtyeight %>%
+spi_matches %>%
   filter(league_id=="2411") %>%
   filter(is.na(xg1)==FALSE) %>%
-  tail(50) %>%
+  top_n(50,date) %>%
   view
+
+spi_matches %>%
+  filter(team1=="Southampton"|team2=="Southampton") %>%
+  view()
