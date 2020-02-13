38b91b2411ca10fe75e139d18eea1d1fcf02ddd9
saintsnumbers <52595717+saintsnumbers@users.noreply.github.com>
2020-02-13 12:59:42 +0000

    comment

--- R/fivethirtyeight/scraper.R
+++ R/fivethirtyeight/scraper.R
@@ -7,12 +7,12 @@ spi_leagues <- spi_matches %>%
   select(league,league_id) %>%
   unique()
 
-spi_matches %>%
-  filter(league_id=="2411") %>%
-  filter(is.na(xg1)==FALSE) %>%
-  top_n(50,date) %>%
-  view
+# spi_matches %>%
+#   filter(league_id=="2411") %>%
+#   filter(is.na(xg1)==FALSE) %>%
+#   top_n(50,date) %>%
+#   view
 
-spi_matches %>%
-  filter(team1=="Southampton"|team2=="Southampton") %>%
-  view()
+# spi_matches %>%
+#   filter(team1=="Southampton"|team2=="Southampton") %>%
+#   view()
