694237a7f56d5d32066931383d5e52e7723b3896
saintsnumbers <52595717+saintsnumbers@users.noreply.github.com>
2020-02-14 18:28:22 +0000

    remove code that is in tidy now

--- R/join/join.R
+++ R/join/join.R
@@ -1,17 +1,14 @@
-raw <- readRDS(file=here("data","fbref","raw.rds"))
-tidy <- list()
-source(here("R","fbref","tidy-players.R"))
-source(here("R","fbref","tidy-teams.R"))
-source(here("R","fbref","tidy-matches.R"))
-
+# players
 players <- reduce(tidy[["player"]],full_join) %>%
   separate("Player",c("Player",NA),sep="\\\\",fill="right") %>%
   separate("Nation",c(NA,"Nation"),sep=" ",fill="right") %>%
   separate("Pos",c("Pos1",NA,"Pos2"),sep=c(2,3),fill="right")
 
+# squad
 squad <- tidy[["table"]] %>%
   full_join(reduce(tidy[["squad"]],full_join))
 
+# matches
 matches <- tidy[["matches"]]
 
 matches_long <- tidy[["matches"]] %>%
