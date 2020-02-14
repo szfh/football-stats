e6481c0d4f00bceb88a1d8bb3530821334642dcd
saintsnumbers <52595717+saintsnumbers@users.noreply.github.com>
2020-02-14 18:26:29 +0000

    source to join

--- R/master.R
+++ R/master.R
@@ -3,11 +3,14 @@ source(here::here("R","library.R"))
 source(here("R","themes.R"))
 
 # raw
-source(here("R","fbref","raw.R"))
+# source(here("R","fbref","raw.R"))
 # source(here("R","fivethirtyeight","raw.R"))
 
 # tidy
-# source(here("R","fbref","tidy.R"))
+source(here("R","fbref","tidy.R"))
 # source(here("R","fivethirtyeight","tidy.R"))
+
+# join
+source(here("R","join.R"))
 
 # graph
