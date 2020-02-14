3805403fda197a1775e7779689e0839cb3261c03
saintsnumbers <52595717+saintsnumbers@users.noreply.github.com>
2020-02-14 18:09:38 +0000

    raw and tidy

--- R/master.R
+++ R/master.R
@@ -3,7 +3,11 @@ source(here::here("R","library.R"))
 source(here("R","themes.R"))
 
 # raw
+source(here("R","fbref","raw.R"))
+# source(here("R","fivethirtyeight","raw.R"))
 
 # tidy
+# source(here("R","fbref","tidy.R"))
+# source(here("R","fivethirtyeight","tidy.R"))
 
 # graph
