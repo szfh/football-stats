55ef3f5129974563b38e0d50c91bb8c0b9dfe60c
saintsnumbers <52595717+saintsnumbers@users.noreply.github.com>
2020-02-14 20:34:17 +0000

    assign to raw list

--- R/fivethirtyeight/raw.R
+++ R/fivethirtyeight/raw.R
@@ -1,18 +1,28 @@
-library(tidyverse)
+source(here::here("R","library.R"))
+
+if (!exists("raw",inherits=FALSE)){
+  raw <- list()
+}
 
 spi_matches <- read.csv("https://projects.fivethirtyeight.com/soccer-api/club/spi_matches.csv")
 spi_rankings <- read.csv("https://projects.fivethirtyeight.com/soccer-api/club/spi_global_rankings.csv")
 
-spi_leagues <- spi_matches %>%
-  select(league,league_id) %>%
-  unique()
 
-# spi_matches %>%
+
+raw[["fivethirtyeight"]][["matches"]] <- read.csv("https://projects.fivethirtyeight.com/soccer-api/club/spi_matches.csv")
+raw[["fivethirtyeight"]][["rankings"]] <- read.csv("https://projects.fivethirtyeight.com/soccer-api/club/spi_global_rankings.csv")
+
+# raw[["fivethirtyeight"]][["matches"]] %>%
+#   select(league,league_id) %>%
+#   unique() %>% 
+#   view()
+
+# raw[["fivethirtyeight"]][["matches"]] %>%
 #   filter(league_id=="2411") %>%
 #   filter(is.na(xg1)==FALSE) %>%
 #   top_n(50,date) %>%
-#   view
+#   view()
 
-# spi_matches %>%
+# raw[["fivethirtyeight"]][["matches"]] %>%
 #   filter(team1=="Southampton"|team2=="Southampton") %>%
 #   view()
