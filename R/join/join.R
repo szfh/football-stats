bce22ca2db839427f1ed1741749aaf25fb7d3a25
saintsnumbers <52595717+saintsnumbers@users.noreply.github.com>
2020-02-14 21:30:40 +0000

    new lists

--- R/join/join.R
+++ R/join/join.R
@@ -1,17 +1,18 @@
 # players
-players <- reduce(tidy[["player"]],full_join) %>%
+players <- reduce(tidy[["fbref"]][["player"]],full_join) %>%
   separate("Player",c("Player",NA),sep="\\\\",fill="right") %>%
   separate("Nation",c(NA,"Nation"),sep=" ",fill="right") %>%
   separate("Pos",c("Pos1",NA,"Pos2"),sep=c(2,3),fill="right")
 
 # squad
-squad <- tidy[["table"]] %>%
-  full_join(reduce(tidy[["squad"]],full_join))
+squad <- tidy[["fbref"]][["table"]] %>%
+  full_join(reduce(tidy[["fbref"]][["squad"]],full_join))
 
 # matches
-matches <- tidy[["matches"]]
+matches <- tidy[["fbref"]][["matches"]] %>%
+  left_join(tidy[["fivethirtyeight"]][["matches"]])
 
-matches_long <- tidy[["matches"]] %>%
+matches_long <- matches %>%
   pivot_longer(cols=c(Home,Away),
                names_to="HA",
                values_to="Team") %>%
@@ -28,4 +29,15 @@ matches_long <- tidy[["matches"]] %>%
     -("Home":"Away"),
   )
 
-rm(raw,tidy)
+# matches_join <- matches %>%
+#   left_join(spi_matches_rn) %>%
+#   filter(!is.na(score1)) %>%
+#   # select(Home) %>%
+#   # unique() %>%
+#   view
+
+# rm(raw,tidy)
+
+# matches_long %>%
+#   filter(Team=="Southampton") %>%
+#   view
