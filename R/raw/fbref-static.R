6f169e3f3bd42c8ebf99df041a18cd1b01d9b4e2
szfh <52595717+szfh@users.noreply.github.com>
2020-05-03 11:45:39 +0100

    nested lists

--- R/raw/fbref-static.R
+++ R/raw/fbref-static.R
@@ -1,10 +1,10 @@
-old_matches <- list()
+raw <- list()
 
-old_matches[["2016"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/1889/schedule/2016-2017-Premier-League-Fixtures",
+raw[["fbref"]][["matches"]][["2016"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/1889/schedule/2016-2017-Premier-League-Fixtures",
                                       extract=1,fix_columns=FALSE)
-old_matches[["2017"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/1889/schedule/2017-2018-Premier-League-Fixtures",
+raw[["fbref"]][["matches"]][["2017"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/1889/schedule/2017-2018-Premier-League-Fixtures",
                                       extract=1,fix_columns=FALSE)
-old_matches[["2018"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/1889/schedule/2018-2019-Premier-League-Fixtures",
+raw[["fbref"]][["matches"]][["2018"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/1889/schedule/2018-2019-Premier-League-Fixtures",
                                             extract=1,fix_columns=FALSE)
 
-saveRDS(old_matches,file=here("data","oldmatches-fbref.rds"))
+saveRDS(raw,file=here("data","oldmatches-fbref.rds"))
