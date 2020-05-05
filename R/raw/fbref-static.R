9c3dcb6bf97a9bbe397c501ea424115dc6f25d40
szfh <52595717+szfh@users.noreply.github.com>
2020-05-05 11:08:01 +0100

    fix links

--- R/raw/fbref-static.R
+++ R/raw/fbref-static.R
@@ -1,17 +1,17 @@
 raw <- list()
 
-raw[["fbref"]][["table"]][["2016"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/1889/2016-2017-Premier-League-Stats",
+raw[["fbref"]][["table"]][["2016"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/1526/",
                                                     extract=1,fix_columns=FALSE)
-raw[["fbref"]][["table"]][["2017"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/1889/2017-2018-Premier-League-Stats",
+raw[["fbref"]][["table"]][["2017"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/1631/",
                                                     extract=1,fix_columns=FALSE)
-raw[["fbref"]][["table"]][["2018"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/1889/2018-2019-Premier-League-Stats",
+raw[["fbref"]][["table"]][["2018"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/1889/",
                                                     extract=1,fix_columns=FALSE)
 
-raw[["fbref"]][["matches"]][["2016"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/1889/schedule/2016-2017-Premier-League-Fixtures",
+raw[["fbref"]][["matches"]][["2016"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/1526/schedule/",
                                                       extract=1,fix_columns=FALSE)
-raw[["fbref"]][["matches"]][["2017"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/1889/schedule/2017-2018-Premier-League-Fixtures",
+raw[["fbref"]][["matches"]][["2017"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/1631/schedule/",
                                                       extract=1,fix_columns=FALSE)
-raw[["fbref"]][["matches"]][["2018"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/1889/schedule/2018-2019-Premier-League-Fixtures",
+raw[["fbref"]][["matches"]][["2018"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/1889/schedule/",
                                                       extract=1,fix_columns=FALSE)
 
 saveRDS(raw,file=here("data","fbref-static.rds"))
