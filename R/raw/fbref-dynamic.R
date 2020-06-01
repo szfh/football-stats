bef3d022fef400ede571e3b9f493a22d17e4f17f
szfh <52595717+szfh@users.noreply.github.com>
2020-06-01 20:07:58 +0100

    scrape dynamic fbref pages

--- R/raw/fbref-dynamic.R
+++ R/raw/fbref-dynamic.R
@@ -1,57 +1,38 @@
-fbref_raw <- readRDS(file=here("data","fbref-raw-static.rds"))
-fbref <- readRDS(file=here("data","fbref-test.rds"))
-# 
-# raw[["fbref"]][["table"]][["2019"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/Premier-League-Stats",
-#                                           extract=1,fix_columns=FALSE)
-# 
-# raw[["fbref"]][["matches"]][["2019"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/schedule/Premier-League-Fixtures",
-#                                                       extract=1,fix_columns=FALSE)
-# 
-# raw[["fbref"]][["squad"]][["stats"]][["2019"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/stats/Premier-League-Stats",
-#                                                      extract=1,fix_columns=TRUE)
-# raw[["fbref"]][["squad"]][["keepers"]][["2019"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/keepers/Premier-League-Stats",
-#                                                        extract=1,fix_columns=TRUE)
-# raw[["fbref"]][["squad"]][["keepersadv"]][["2019"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/keepersadv/Premier-League-Stats",
-#                                                           extract=1,fix_columns=TRUE)
-# raw[["fbref"]][["squad"]][["shooting"]][["2019"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/shooting/Premier-League-Stats",
-#                                                         extract=1,fix_columns=TRUE)
-# raw[["fbref"]][["squad"]][["passing"]][["2019"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/passing/Premier-League-Stats",
-#                                                        extract=1,fix_columns=TRUE)
-# raw[["fbref"]][["squad"]][["passingtypes"]][["2019"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/passing_types/Premier-League-Stats",
-#                                                             extract=1,fix_columns=TRUE)
-# raw[["fbref"]][["squad"]][["gca"]][["2019"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/gca/Premier-League-Stats",
-#                                                    extract=1,fix_columns=TRUE)
-# raw[["fbref"]][["squad"]][["defense"]][["2019"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/defense/Premier-League-Stats",
-#                                                        extract=1,fix_columns=TRUE)
-# raw[["fbref"]][["squad"]][["possession"]][["2019"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/possession/Premier-League-Stats",
-#                                                           extract=1,fix_columns=TRUE)
-# raw[["fbref"]][["squad"]][["playingtime"]][["2019"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/playingtime/Premier-League-Stats",
-#                                                            extract=1,fix_columns=TRUE)
-# raw[["fbref"]][["squad"]][["misc"]][["2019"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/misc/Premier-League-Stats",
-#                                                     extract=1,fix_columns=TRUE)
-# 
-# raw[["fbref"]][["player"]][["stats"]][["2019"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/stats/Premier-League-Stats",
-#                                                       comment=TRUE,extract=1,fix_columns=TRUE)
-# raw[["fbref"]][["player"]][["keepers"]][["2019"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/keepers/Premier-League-Stats",
-#                                                         comment=TRUE,extract=1,fix_columns=TRUE)
-# raw[["fbref"]][["player"]][["keepersadv"]][["2019"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/keepersadv/Premier-League-Stats",
-#                                                            comment=TRUE,extract=1,fix_columns=TRUE)
-# raw[["fbref"]][["player"]][["shooting"]][["2019"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/shooting/Premier-League-Stats",
-#                                                          comment=TRUE,extract=1,fix_columns=TRUE)
-# raw[["fbref"]][["player"]][["passing"]][["2019"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/passing/Premier-League-Stats",
-#                                                         comment=TRUE,extract=1,fix_columns=TRUE)
-# raw[["fbref"]][["player"]][["passingtypes"]][["2019"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/passing_types/Premier-League-Stats",
-#                                                              comment=TRUE,extract=1,fix_columns=TRUE)
-# raw[["fbref"]][["player"]][["gca"]][["2019"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/gca/Premier-League-Stats",
-#                                                     comment=TRUE,extract=1,fix_columns=TRUE)
-# raw[["fbref"]][["player"]][["defense"]][["2019"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/defense/Premier-League-Stats",
-#                                                         comment=TRUE,extract=1,fix_columns=TRUE)
-# raw[["fbref"]][["player"]][["possession"]][["2019"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/possession/Premier-League-Stats",
-#                                                            comment=TRUE,extract=1,fix_columns=TRUE)
-# raw[["fbref"]][["player"]][["playingtime"]][["2019"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/playingtime/Premier-League-Stats",
-#                                                             comment=TRUE,extract=1,fix_columns=TRUE)
-# raw[["fbref"]][["player"]][["misc"]][["2019"]] <- fbref_scrape(url="https://fbref.com/en/comps/9/misc/Premier-League-Stats",
-#                                                      comment=TRUE,extract=1,fix_columns=TRUE)
-# 
-# saveRDS(raw,file=here("data","raw-fbref.rds"))
-# rm(raw)
+fbref_static <- readRDS(file=here("data","fbref-raw-static.rds"))
+
+.EPLseasons <- tribble(~Season, ~Code, # advanced/nonadvanced?
+                       "2019-20",NA
+)
+
+.Pages <- tribble(~Page, ~Table,
+                  "stats","standard",
+                  "keepers","keeper",
+                  "keepersadv","keeper_adv",
+                  "shooting","shooting",
+                  "passing","passing",
+                  "passing_types","passing_types",
+                  "gca","gca",
+                  "defense","defense",
+                  "possession","possession",
+                  "playingtime","playing_time",
+                  "misc","misc",
+)
+
+.Datatype <- tribble(~Type, ~Selector,
+                     "squad","_squads",
+                     "player","")
+
+fbref <- .EPLseasons %>%
+  crossing(.Pages) %>%
+  crossing(.Datatype)
+
+fbref %<>%
+  mutate(page_url=glue("https://fbref.com/en/comps/9/{Page}/")) %>%
+  mutate(content_selector_id=glue("%23stats_{Table}{Selector}")) %>%
+  mutate(data = map2(page_url, content_selector_id, possibly(fbref_scrape, otherwise=NA))) %>%
+  print
+
+fbref <- bind_rows(fbref_static,fbref)
+
+saveRDS(fbref,file=here("data","fbref-raw.rds"))
+rm(fbref_static,fbref)
