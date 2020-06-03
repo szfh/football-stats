abbb6fb057c01913f2e505582c1a4bb1fd968cf5
szfh <52595717+szfh@users.noreply.github.com>
2020-06-03 16:27:16 +0100

    back to lower case

--- R/raw/fbref-static.R
+++ R/raw/fbref-static.R
@@ -1,10 +1,10 @@
-.EPLseasons <- tribble(~Season, ~Code, # advanced/nonadvanced?
+.eplseasons <- tribble(~season, ~code, # advanced/nonadvanced?
                        "2018-19",1889,
                        "2017-18",1631,
                        # "2016-17",1526,
 )
 
-.Pages <- tribble(~Page, ~Table,
+.pages <- tribble(~page, ~table,
                   "stats","standard",
                   "keepers","keeper",
                   "keepersadv","keeper_adv",
@@ -18,17 +18,17 @@
                   "misc","misc",
 )
 
-.Datatype <- tribble(~Type, ~Selector,
+.datatype <- tribble(~type, ~selector,
                      "squad","_squads",
                      "player","")
 
-fbref <- .EPLseasons %>%
-  crossing(.Pages) %>%
-  crossing(.Datatype)
+fbref <- .eplseasons %>%
+  crossing(.pages) %>%
+  crossing(.datatype)
 
 fbref %<>%
-  mutate(page_url=glue("https://fbref.com/en/comps/9/{Code}/{Page}/")) %>%
-  mutate(content_selector_id=glue("%23stats_{Table}{Selector}")) %>%
+  mutate(page_url=glue("https://fbref.com/en/comps/9/{code}/{page}/")) %>%
+  mutate(content_selector_id=glue("%23stats_{table}{selector}")) %>%
   mutate(data = map2(page_url, content_selector_id, possibly(fbref_scrape, otherwise=NA)))
 
 saveRDS(fbref,file=here("data","fbref-raw-static.rds"))
