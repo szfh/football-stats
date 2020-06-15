cfa67f1755399c443aaf3f5171cd1cc9ca8ee197
szfh <52595717+szfh@users.noreply.github.com>
2020-06-15 20:59:28 +0100

    join matches

--- R/join/join.R
+++ R/join/join.R
@@ -1,26 +1,30 @@
 fbref <- readRDS(file=here("data","fbref-tidy.rds"))
 
 squad <- fbref %>%
-  filter(type=="squad") %>%
-  select(-type) %>%
+  filter(page=="squad") %>%
+  select(-page) %>%
   unnest(cols=data) %>%
-  group_by(page) %>%
+  group_by(stattype) %>%
   nest() %>%
-  ungroup() %>%
+  mutate(data=map(data,remove_empty,which="cols")) %$%
+  data %>%
+  reduce(full_join) %>%
+  print
+
+players <- fbref %>%
+  filter(page=="player") %>%
+  select(-page) %>%
+  unnest(cols=data) %>%
+  group_by(stattype) %>%
+  nest() %>%
   mutate(data=map(data,remove_empty,which="cols")) %$%
   data %>%
   reduce(full_join)
 
-players <- fbref %>%
-  filter(type=="player") %>%
-  select(-type) %>%
-  unnest(cols=data) %>%
-  group_by(page) %>%
-  nest() %>%
-  ungroup() %>%
-  mutate(data=map(data,remove_empty,which="cols")) %$%
-  data %>%
-  reduce(full_join)
+matches <- fbref %>%
+  filter(page=="schedule") %>%
+  select(-page,-stattype) %>%
+  unnest(cols=data)
 
 # # read data
 # tidy <- c(
