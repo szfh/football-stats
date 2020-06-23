366166416e243ffdc985385e45f7ab566d4b177c
szfh <52595717+szfh@users.noreply.github.com>
2020-06-23 22:15:33 +0100

    only scrape new data

--- R/raw/raw-understat.R
+++ R/raw/raw-understat.R
@@ -2,7 +2,7 @@ source(here("R","raw","raw-utils.R"))
 
 .eplseasons <- tribble(~season,
                        "2019",
-                       # "2018",
+                       "2018",
 )
 
 .datatypes_1 <- tribble(~datatype,
@@ -17,13 +17,20 @@ source(here("R","raw","raw-utils.R"))
 
 understat_saved <- readRDS(here("data","understat-raw.rds"))
 
-understat <- data.frame() %>%
+understat_all <- data.frame() %>%
   bind_rows(crossing(.datatypes_1,.tables_1)) %>%
   crossing(.eplseasons) %>%
   print
 
-understat %<>%
+understat_keep <- understat_saved %>%
+  filter(season!=2019)
+
+understat_new <-
+  anti_join(understat_all, understat_keep) %>%
   mutate(data=pmap(list("EPL",season,statselector),possibly(understat_scrape_league, otherwise=NA))) %>%
   print
+
+understat <- bind_rows(understat_keep, understat_new) %>%
+  filter(!is.na(data))
 
 saveRDS(understat,here("data","understat-raw.rds"))
