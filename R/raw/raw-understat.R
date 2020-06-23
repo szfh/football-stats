b603ab3d0ce53149fd5895e0528a9fb993b23d72
szfh <52595717+szfh@users.noreply.github.com>
2020-06-23 21:54:16 +0100

    use anon

--- R/raw/raw-understat.R
+++ R/raw/raw-understat.R
@@ -1,24 +1,24 @@
 source(here::here("R","library.R"))
 source(here("R","raw","raw-utils.R"))
 
-eplseasons <- tribble(~season,
+.eplseasons <- tribble(~season,
                        "2019",
                        "2018",
 )
 
-datatypes_1 <- tribble(~datatype,
+.datatypes_1 <- tribble(~datatype,
                        "league",
 )
 
-tables_1 <- tribble(~stattype, ~statselector,
+.tables_1 <- tribble(~stattype, ~statselector,
                      "schedule","datesData",
                      "players","playersData",
                     #"teams","teamsData"
 )
 
 understat <- data.frame() %>%
-  bind_rows(crossing(datatypes_1,tables_1)) %>%
-  crossing(eplseasons) %>%
+  bind_rows(crossing(.datatypes_1,.tables_1)) %>%
+  crossing(.eplseasons) %>%
   print
 
 understat %<>%
