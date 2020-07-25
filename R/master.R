89c0c914b2de9e7fcd00740201d9037c4a27a25e
szfh <52595717+szfh@users.noreply.github.com>
2020-07-25 09:47:11 +0100

    rebuild master script

--- R/master.R
+++ R/master.R
@@ -1,27 +1,21 @@
 # libraries
 # source(here::here("R","library.R"))
-# source(here("R","master-utils.R"))
-
-# themes
-# source(here("R","themes.R"))
 
 # knit markdown
 # knitr::knit(here::here("R","README.Rmd"))
 
-# raw static
+# raw
 # source(here("R","raw","raw-fbref.R"))
-# source(here("R","raw","understat-static.R"))
 
-# raw dynamic
-# source(here("R","raw","fivethirtyeight.R"))
-
-# tidy
-# source(here("R","tidy","tidy-fbref.R"))
-# source(here("R","tidy","fivethirtyeight.R"))
-
-# join
+# plot
 # source(here("R","join","join.R"))
-
-# graph
+# source(here("R","themes.R"))
 # source(here("R","plot","plot-sfc.R"))
 # source(here("R","plot","plot-epl.R"))
+
+# old
+# source(here("R","master-utils.R"))
+# source(here("R","raw","understat-static.R"))
+# source(here("R","raw","fivethirtyeight.R"))
+# source(here("R","tidy","tidy-fbref.R"))
+# source(here("R","tidy","fivethirtyeight.R"))
