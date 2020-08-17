8d98eb903c6c266d46ec27178c2d43ecd4fd3d6d
szfh <52595717+szfh@users.noreply.github.com>
2020-08-17 21:53:33 +0100

    import 2020 team data

--- R/raw/raw-canpl.R
+++ R/raw/raw-canpl.R
@@ -1,5 +1,15 @@
 source(here("R","raw","raw-utils.R"))
+library(googlesheets4)
 
 import_canpl <- function(save_path=here("data","canpl-raw.rds")){
-  NULL
+  
+  canpl <- tribble(~season, ~table, ~url,
+                   "2020","Team Totals","1QaFAXjW6O68gpMFLRIBVf-EdIYeKznqvNQQy6cc5DNM",
+  )
+  
+  canpl %<>%
+    mutate(data=map(url,googlesheets4::read_sheet))
+  
 }
+
+canpl <- import_canpl()
