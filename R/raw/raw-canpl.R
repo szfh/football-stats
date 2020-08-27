1a255fe0734bc847966c8052f4f39c272a538c8e
szfh <52595717+szfh@users.noreply.github.com>
2020-08-27 21:41:11 +0100

    save cpl data as rds

--- R/raw/raw-canpl.R
+++ R/raw/raw-canpl.R
@@ -1,4 +1,4 @@
-import_canpl <- function(save_folder=here("data","CPL")){
+import_canpl <- function(save_folder=here("data","CPL"),save_path=here("data","canpl-raw.rds")){
   
   find_cc <- drive_find(pattern="Centre Circle") #auto select account?
   get_cc <- drive_get(as_id(find_cc[1,]))
@@ -9,6 +9,17 @@ import_canpl <- function(save_folder=here("data","CPL")){
   for(i in 1:dim(csv_files)[1]){
     drive_download(file=csv_files[i,],path=as.character(csv_files[i,"path"]),overwrite=TRUE)
   }
+  
+  csv_local <- dir(path=save_folder,pattern=".csv")
+  
+  canpl <- tibble(name=csv_local) %>%
+    mutate(path=glue("{save_folder}/{csv_local}")) %>%
+    mutate(data=map(path,read_csv))
+  
+  saveRDS(canpl,file=save_path)
+  
+  return(canpl)
+  
 }
 
 # csv_files %>%
