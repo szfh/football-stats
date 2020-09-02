cea3fcf7e1c049586000ec1168b5348558f6a19f
szfh <52595717+szfh@users.noreply.github.com>
2020-09-02 21:59:52 +0100

    join-utils in join.R

--- R/join/join.R
+++ R/join/join.R
@@ -1,4 +1,4 @@
-source(here("R","join","join-utils.R"))
+# source(here("R","join","join-utils.R"))
 # fbref <- readRDS(file=here("data","fbref-raw.rds"))
 # understat <- readRDS(file=here("data","understat-raw.rds"))
 
@@ -70,3 +70,66 @@ join <- function(){
   data <- list("table"=table,"squad"=squad,"players"=players,"matches"=matches,"shots"=shots,"match_stats"=match_stats,"canpl"=canpl)
   return(data)
 }
+
+fbref_clean_names <- function(data,page){
+  if(page %in% c("squad","player","leagueha")){
+    names(data) <-
+      glue("{data[1,]} {data[2,]}") %>%
+      str_squish() %>%
+      str_to_lower() %>%
+      str_replace_all(c(" "="_","%"="pc","#"="n")) %>%
+      str_remove_all("[/ \\( \\)]") %>%
+      make.unique(sep="_") %>%
+      print
+    
+    data %<>% slice(-1,-2)
+  }
+  if(page %in% c("schedule","league")){
+    names(data) <-
+      glue("{data[1,]}") %>%
+      str_squish() %>%
+      str_to_lower() %>%
+      str_replace_all(c(" "="_","%"="pc","#"="n")) %>%
+      str_remove_all("[/ \\( \\)]") %>%
+      make.unique(sep="_") %>%
+      print
+    
+    data %<>% slice(-1)
+  }
+  if("player" %in% names(data)){ # remove duplicated column names from player table
+    data %<>% filter(player != "Player")
+  }
+  if("wk" %in% names(data)){ # remove duplicated column names + blank rows from schedule
+    data %<>% filter(wk != "Wk") %>% filter(wk != "")
+  }
+  
+  data %<>% type_convert # refactor data types
+  
+  return(data)
+}
+
+fbref_tidy <- function(data,page,stattype){
+  
+  if(page %in% c("squad","player","schedule","league","leagueha")){
+    data %<>%
+      select(-any_of(c("rk","matches","notes","match_report","top_team_scorer","goalkeeper"))) %>%
+      select(-contains(c("pc","90")))
+  }
+  if(page=="player"){
+    data %<>%
+      separate("nation",c(NA,"nation"),sep=" ",fill="right") %>%
+      separate("pos",c("pos1",NA,"pos2"),sep=c(2,3),fill="right")
+  }
+  if(page=="schedule"){
+    data %<>%
+      separate("score",c("homegls","awaygls"),sep="[:punct:]",fill="right") %>%
+      rename("homexg"="xg","awayxg"="xg_1")
+  }
+  if(stattype %in% c("keepers","keepersadv")){
+    data %<>%
+      rename("n_pl_gk"=any_of("n_pl")) %>%
+      select(-any_of(c("playing_time_starts","playing_time_mp","playing_time_min")))
+  }
+  return(data)
+}
+
