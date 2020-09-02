7a4e7533a33119a229405b32abd69e6716b7f9cb
szfh <52595717+szfh@users.noreply.github.com>
2020-09-02 20:47:44 +0100

    tidy

--- R/raw/raw-fbref.R
+++ R/raw/raw-fbref.R
@@ -1,5 +1,5 @@
 scrape_fbref <- function(save_path=here("data","fbref-raw.rds")){
-
+  
   fbref_saved <- readRDS(save_path)
   
   eplseasons <- tribble(~season, ~seasoncode, #advanced/non-advanced?
@@ -53,7 +53,7 @@ scrape_fbref <- function(save_path=here("data","fbref-raw.rds")){
   
   fbref <- bind_rows(fbref_keep,fbref_new) %>%
     filter(!is.na(data))
-
+  
   saveRDS(fbref,file=save_path)
   
   return(fbref)
@@ -62,9 +62,7 @@ scrape_fbref <- function(save_path=here("data","fbref-raw.rds")){
 fbref_get_selector <- function(page,seasoncode=NA,stattype=NA,statselector=NA){
   
   selector <- case_when(
-    # page=="player" && stattype=="stats" ~ glue("%23standard"),
     page=="player" ~ glue("%23stats_{statselector}"),
-    # page=="squad" && stattype=="stats" ~ glue("%23standard_squads"),
     page=="squad" ~ glue("%23stats_{statselector}_squads"),
     page=="schedule" ~ glue("%23sched_ks_{seasoncode}_1"),
     page=="league" ~ glue("%23results{seasoncode}1_overall"),
@@ -89,20 +87,11 @@ fbref_scrape <- function(page_url=NA,content_selector_id=NA,page=NA,stattype=NA)
   url <- glue("http://acciotables.herokuapp.com/?page_url={page_url}&content_selector_id={content_selector_id}")
   print(glue("url: {url}"))
   
-  Sys.sleep(2)
-  
   # import table
   data_html <-
     url %>%
     read_html()
   
-  # read table
-  # data_table <-
-  #   data_html %>%
-  #   html_table(header=FALSE) %>%
-  #   extract2(1)
-  
-  # browser()
   session <- polite::bow(url,user_agent="@saintsbynumbers")
   data_html <- polite::scrape(session)
   
@@ -117,12 +106,9 @@ fbref_scrape <- function(page_url=NA,content_selector_id=NA,page=NA,stattype=NA)
   data <- fbref_scrape_href(data_html,data_table,page)
   
   return(data)
-  
 }
 
 fbref_scrape_href <- function(data_html,data_table,page=NA){
-  # browser()
-  
   if(page %in% c("league")){
     data_href <-
       data_html %>%
@@ -145,9 +131,7 @@ fbref_scrape_href <- function(data_html,data_table,page=NA){
       filter(datatype=="matches") %>%
       filter(!is.na(desc)) %>%
       unique()
-    
-    # d <- nrow(data_table) - nrow(data_href)
-    
+
     if(nrow(data_table) - nrow(data_href) > 0){
       data_href <- data_href %>%
         add_row(code=(rep(NA,nrow(data_table) - nrow(data_href)))) #extend df
@@ -156,91 +140,8 @@ fbref_scrape_href <- function(data_html,data_table,page=NA){
   } else {
     data <- data_table
   }
-  
   return(data)
 }
-
-# fbref_scrape <- function(page_url,content_selector_id, datatype){
-#   url <- glue("http://acciotables.herokuapp.com/?page_url={page_url}&content_selector_id={content_selector_id}")
-#   print(glue("url: {url}"))
-#   
-#   # browser()
-#   
-#   data <- case_when(
-#     datatype=="urlcode" ~ glue("datatype = urlcode!"),
-#     TRUE ~
-#       url %>%
-#       read_html() %>%
-#       html_table(header=FALSE) %>%
-#       extract2(1)
-#   )
-#   
-#   print(data)
-#   
-#   # data <-
-#   #   url %>%
-#   #   read_html() %>%
-#   #   html_table(header=FALSE) %>%
-#   #   extract2(1)
-#   
-#   return(data)
-# }
-
-# fbref_scrape_href <- function(page_url,content_selector_id){
-#   url <- glue("http://acciotables.herokuapp.com/?page_url={page_url}&content_selector_id={content_selector_id}")
-#   print(glue("url: {url}"))
-#   
-#   data <-
-#     url %>%
-#     read_html() %>%
-#     html_nodes("a") %>%
-#     html_attr("href") %>%
-#     as_tibble() %>%
-#     print
-#   
-#   return(data)
-# }
-
-# fbref_get_codes_squads <- function(eplseasons){
-#   
-#   # browser()
-#   
-#   data <- tibble(page="league") %>%
-#     crossing(.eplseasons) %>%
-#     mutate(page_url=fbref_get_url(page,seasoncode)) %>%
-#     mutate(content_selector_id=fbref_get_selector(page,seasoncode)) %>%
-#     mutate(data=map2(page_url,content_selector_id,fbref_scrape_href)) %>%
-#     unnest(cols=data) %>%
-#     separate(value,c(NA,NA,"datatype","code","desc"),sep="/",extra="merge",fill="right") %>%
-#     separate(desc,c(NA,"desc"),sep="/",fill="left") %>%
-#     filter(datatype=="squads") %>%
-#     select(season,datatype,code,desc)
-#   
-#   return(data)
-# }
-
-# fbref_get_codes_matches <- function(eplseasons){
-#   
-#   data <- tibble(page="schedule") %>%
-#     crossing(.eplseasons) %>%
-#     mutate(page_url=fbref_get_url(page,seasoncode)) %>%
-#     mutate(content_selector_id=fbref_get_selector(page,seasoncode)) %>%
-#     mutate(data=map2(page_url,content_selector_id,fbref_scrape_href)) %>%
-#     # unnest(cols=data) %>%
-#     # separate(value,c(NA,NA,"datatype","code","data"),sep="/",extra="merge",fill="right") %>%
-#     # separate(data,c(NA,"data"),sep="/",fill="left") %>%
-#     # filter(datatype=="squads") %>%
-#     unnest(cols=data) %>%
-#     separate(value,c(NA,NA,"datatype","code","desc"),sep="/",extra="merge",fill="right") %>%
-#     filter(!is.na(desc)) %>%
-#     filter(datatype=="matches") %>%
-#     unique() %>%
-#     # print
-#     select(season,datatype,code,desc) %>%
-#     print
-#   
-#   return(data)
-# }
 
 fbref_clean_names <- function(data,page){
   if(page %in% c("squad","player","leagueha")){
