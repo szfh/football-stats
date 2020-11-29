5e02b8e097d80e7ba807a3a743bb5e6668410308
szfh <52595717+szfh@users.noreply.github.com>
2020-11-29 09:41:55 +0000

    tidy

--- R/join/join.R
+++ R/join/join.R
@@ -3,30 +3,16 @@ join <- function(
   understat=readRDS(file=here("data","understat.rds")),
   canpl=readRDS(file=here("data","canpl.rds"))
 ){
-  
-  browser()
-  
-  # fbref_join <- join_fbref(fbref)
   fbref_join <- possibly(join_fbref, otherwise=NA)(fbref)
-  # understat_join <- join_understat(understat)
   understat_join <- possibly(join_understat, otherwise=NA)(understat)
-  # canpl_join <- join_canpl(canpl)
   canpl_join <- possibly(join_fbref, otherwise=NA)(canpl)
-  # possibly(join_understat(understat),otherwise=NA)
-  # possibly(join_canpl(canpl),otherwise=NA)
   
   data <- list(fbref=fbref_join,understat=understat_join,canpl=canpl_join)
   
-  browser()
-  
   return(data)
-  
 }
 
 join_fbref <- function(fbref){
-  
-  # browser()
-  
   data <- list()
   
   fbref <-
@@ -72,14 +58,11 @@ join_fbref <- function(fbref){
     filter(page=="schedule") %>%
     select(-page,-stattype) %>%
     unnest(cols=data)
-   
-  # browser()
   
   return(data)
 }
 
 join_understat <- function(understat){
-  
   data <- list()
   
   understat <-
@@ -110,7 +93,6 @@ join_understat <- function(understat){
 }
 
 join_canpl <- function(canpl){
-  
   data <- list()
   
   canpl <-
@@ -119,98 +101,6 @@ join_canpl <- function(canpl){
   
   data$canpl <-
     canpl
-  
-  return(data)
-}
-
-join2 <- function(){
-  fbref <- readRDS(file=here("data","fbref.rds"))
-  understat <- readRDS(file=here("data","understat.rds"))
-  canpl <- readRDS(file=here("data","canpl.rds"))
-  
-  data <- list()
-  
-  # tidy  
-  fbref <-
-    fbref %>%
-    mutate(data=pmap(list(data,page,stattype), possibly(fbref_tidy, otherwise=NA))) %>%
-    select(-any_of(c("statselector","seasoncode","page_url","content_selector_id")))
-  
-  understat <-
-    understat %>%
-    # mutate(data=pmap(function_here)) %>% # make understat_tidy?
-    select(-any_of(c("statselector")))
-  
-  canpl <-
-    canpl %>%
-    select(-any_of(c("path")))
-  
-  # join
-  data$table <-
-    fbref %>%
-    filter(page=="league") %>%
-    select(-page,-stattype) %>%
-    unnest(cols=data)
-  
-  data$squad <-
-    fbref %>%
-    filter(page=="squad") %>%
-    select(-page) %>%
-    unnest(cols=data) %>%
-    group_by(stattype) %>%
-    nest() %>%
-    mutate(data=map(data,remove_empty,which="cols")) %$%
-    data %>%
-    reduce(full_join) %>%
-    mutate(vs=str_detect(squad,"vs "))
-  
-  data$squad <-
-    data$table %>%
-    full_join(data$squad)
-  
-  data$players <-
-    fbref %>%
-    filter(page=="player") %>%
-    select(-page) %>%
-    unnest(cols=data) %>%
-    group_by(stattype) %>%
-    nest() %>%
-    mutate(data=map(data,remove_empty,which="cols")) %$%
-    data %>%
-    reduce(full_join)
-  
-  data$matches <-
-    fbref %>%
-    filter(page=="schedule") %>%
-    select(-page,-stattype) %>%
-    unnest(cols=data)
-  
-  data$shots <-
-    understat %>%
-    filter(datatype=="shots") %>%
-    select(data) %>%
-    unnest(data)
-  
-  data$match_stats <-
-    understat %>%
-    filter(datatype=="stats") %>%
-    select(data) %>%
-    unnest(data)
-  
-  data$us_schedule <- 
-    understat %>%
-    filter(stattype=="schedule") %>%
-    select(-id,-isResult) %>%
-    unnest(cols="data") %>%
-    mutate(match_id=id)
-  
-  data$canpl <-
-    canpl
-  
-  #type convert
-  # data <-
-  #   data %>%
-  #   map(type_convert)
   
   return(data)
 }
