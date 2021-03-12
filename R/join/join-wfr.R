3fae3b11e7339d89f394e653fba77eea90905068
szfh <52595717+szfh@users.noreply.github.com>
2021-03-12 14:27:30 +0000

    add advanced match stats

--- R/join/join-wfr.R
+++ R/join/join-wfr.R
@@ -9,17 +9,12 @@ join_wfr <- function(
 }
 
 join_fbref <- function(fbref){
-  data <- list()
-  
-  glimpse(fbref)
-  
-  browser()
   
   fbref_tidy <-
     fbref %>%
-    mutate(data=pmap(list(data,data_type,stat),tidy_fbref)) %>%
-    # select(-any_of(c("stat_key","season_key","page_url","content_selector_id"))) %>%
-    glimpse
+    mutate(data=pmap(list(data,data_type,stat),tidy_fbref))
+  
+  data <- list()
   
   data$table <-
     fbref_tidy %>%
@@ -35,52 +30,46 @@ join_fbref <- function(fbref){
     unnest(cols=data) %>%
     glimpse
   
-  browser()
+  data$season_stat <-
+    fbref_tidy %>%
+    filter(data_type=="season_stat") %>%
+    filter(stat!="league_table") %>%
+    filter(stat!="league_table_home_away") %>%
+    select(-data_type,-stat) %$%
+    data %>%
+    reduce(full_join) %>%
+    glimpse
   
-  # data$squad <-
-  #   fbref %>%
-  #   filter(page=="squad") %>%
-  #   select(-page) %>%
-  #   unnest(cols=data) %>%
-  #   group_by(stat) %>%
-  #   nest() %>%
-  #   mutate(data=map(data,remove_empty,which="cols")) %$%
-  #   data %>%
-  #   reduce(full_join) %>%
-  #   mutate(vs=str_detect(squad,"vs "))
-  # 
-  # data$squad <-
-  #   data$table %>%
-  #   full_join(data$squad)
-  # 
-  # data$players <-
-  #   fbref %>%
-  #   filter(page=="player") %>%
-  #   select(-page) %>%
-  #   unnest(cols=data) %>%
-  #   group_by(stat) %>%
-  #   nest() %>%
-  #   mutate(data=map(data,remove_empty,which="cols")) %$%
-  #   data %>%
-  #   reduce(full_join)
-  # 
-  # data$matches <-
-  #   fbref %>%
-  #   filter(page=="schedule") %>%
-  #   select(-page,-stat,-home,-away) %>%
-  #   unnest(cols=data)
-  # 
-  # data$events <-
-  #   fbref %>%
-  #   filter(stat=="events") %>%
-  #   select(-page) %>%
-  #   unnest(cols=data)
-  # 
-  # data$shots <-
-  #   fbref %>%
-  #   filter(stat=="shots") %>%
-  #   select(-page) %>%
-  #   unnest(cols=data)
+  data$matches <-
+    fbref_tidy %>%
+    filter(data_type=="match_result") %>%
+    select(-stat) %>%
+    unnest(cols=data) %>%
+    glimpse
+
+  data$team_advanced_stats_match <-
+    fbref_tidy %>%
+    filter(data_type=="advanced_stats") %>%
+    filter(team_or_player=="team") %>%
+    unnest(data) %>%
+    group_by(stat) %>%
+    nest() %>%
+    mutate(data=map(data,remove_empty,which="cols")) %>%
+    pull(data) %>%
+    reduce(full_join) %>%
+    glimpse
+  
+  data$player_advanced_stats_match <-
+    fbref_tidy %>%
+    filter(data_type=="advanced_stats") %>%
+    filter(team_or_player=="player") %>%
+    unnest(data) %>%
+    group_by(stat) %>%
+    nest() %>%
+    mutate(data=map(data,remove_empty,which="cols")) %>%
+    pull(data) %>%
+    reduce(full_join) %>%
+    glimpse
   
   return(data)
 }
@@ -95,18 +84,41 @@ tidy_fbref <- function(data,data_type=NA,stat=NA){
   if(data_type=="match_result"){
     data <-
       data %>%
-      as_tibble()
+      as_tibble() %>%
+      select(-contains(c("Attendance","Venue","Referee","Notes")))
   }
-  if(stat %in% c("league_table","league_table_home_away")){
+  if(data_type=="season_stat" && stat %in% c("league_table","league_table_home_away")){
     data <-
       data %>%
       as_tibble() %>%
       select(-contains(c("90","Last.5","Top.Team.Scorer","Goalkeeper","Notes")))
   }
-  else if(data_type=="season_stat"){
+  if(data_type=="season_stat" && stat %in% c("keeper","keeper_adv")){
+    data <-
+      data %>%
+      as_tibble() %>%
+      # select(-any_of(c("Num_Players":"Min_Playing"))) %>%
+      select(-contains(c("Players","Playing","90","Last.5","Top.Team.Scorer","Goalkeeper","Notes")))
+  }
+  if(data_type=="season_stat"){
     data <-
       data %>%
       as_tibble()
+  }
+  
+  if(data_type=="advanced_stats" && stat=="keeper"){
+    # browser()
+    data <-
+      data %>%
+      as_tibble() %>%
+      select(-contains(c("Player","Nation","Age","Min","Att_Passes")))
+  }
+  if(data_type=="advanced_stats"){
+    # browser()
+    data <-
+      data %>%
+      as_tibble() #%>%
+    # select(-contains(c("Min")))
   }
   
   return(data)
