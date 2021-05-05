163aa13672a498946c6d2e2a995a09460534f209
szfh <52595717+szfh@users.noreply.github.com>
2021-05-05 20:17:26 +0100

    add xg for against plot

--- R/plot/plot-league-wfr.R
+++ R/plot/plot-league-wfr.R
@@ -18,7 +18,9 @@ plot_league_wfr <- function(data,season="2020-2021"){
     summarise(across(where(is.numeric),sum,na.rm=TRUE)) %>%
     ungroup() %>%
     make_long_data(levels=c("npG","npxG"),labels=c("Goals","Expected Goals")) %>%
-    mutate(focus=case_when(min_rank(desc(n))<=20 ~ TRUE,
+    mutate(focus=case_when(
+      key=="Goals" & min_rank(desc(n))<=10 ~ TRUE,
+      key=="Expected Goals" & min_rank(desc(n))<=20 ~ TRUE,
                            TRUE ~ FALSE)) %>%
     ggplot(aes(x=0.05,y=n,alpha=focus)) +
     geom_text_repel(
@@ -41,6 +43,50 @@ plot_league_wfr <- function(data,season="2020-2021"){
     scale_y_continuous() +
     scale_fill_manual(values=palette[["epl"]]()) +
     scale_alpha_manual(values=c("TRUE"=1,"FALSE"=0.1))
+  
+  penalties <-
+    data$fbref$team_advanced_stats_match %>%
+    select(Match_Date,Home_Team,Away_Team,Home_Away,PKatt) %>%
+    unique() %>%
+    filter(!is.na(PKatt)) %>%
+    pivot_wider(names_from=Home_Away, values_from=PKatt, names_glue="PK_{Home_Away}")
+
+  plots$xg_for_against <-
+    data$fbref$team_advanced_stats_match %>%
+    filter(Season %in% !!season) %>%
+    select(Match_Date,Home_Team,Away_Team,Home_xG,Away_xG,Team,Home_Away) %>%
+    left_join(penalties) %>%
+    mutate(Match_Date=lubridate::parse_date_time(Match_Date,"mdy")) %>%
+    filter(Match_Date>="2021-01-01") %>%
+    mutate(Home_npxG=Home_xG-(0.7*PK_Home)) %>%
+    mutate(Away_npxG=Away_xG-(0.7*PK_Away)) %>%
+    mutate(Team_npxG=ifelse(Home_Away=="Home",Home_npxG,Away_npxG)) %>%
+    mutate(Opposition_npxG=ifelse(Home_Away=="Home",-Away_npxG,-Home_npxG)) %>%
+    select(Team,Team_npxG,Opposition_npxG) %>%
+    group_by(Team) %>%
+    summarise(across(where(is.numeric),sum,na.rm=TRUE)) %>%
+    ungroup() %>%
+    make_long_data(levels=c("Team_npxG","Opposition_npxG"),labels=c("Expected Goals For","Expected Goals Against")) %>%
+    ggplot(aes(x=0.05,y=n)) +
+    geom_text_repel(
+      aes(label=Team),
+      size=2.5,
+      nudge_x=0.3,
+      direction="y",
+      hjust=0,
+      segment.size=0.4,
+      box.padding=0.05
+    ) +
+    geom_point(aes(fill=Team),shape=23,size=3,position=position_jitterdodge(jitter.width=0,jitter.height=0,dodge.width=0.04,seed=2)) +
+    theme[["solarfacet"]]() +
+    facet_wrap("key",scales="free") +
+    labs(
+      title="Expected Goals since 1 January 2021",
+      x=element_blank(),
+      y=element_blank()) +
+    scale_x_continuous(limit=c(0,1)) +
+    scale_y_continuous(breaks=seq(-100,100,5),labels=abs(seq(-100,100,5)),expand=expansion(add=1)) +
+    scale_fill_manual(values=palette[["epl"]]())
   
   plots_logo <-
     plots %>%
