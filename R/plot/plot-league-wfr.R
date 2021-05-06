80d80545d6446b1dd9eff15b90b3f1252a21ebb8
szfh <52595717+szfh@users.noreply.github.com>
2021-05-06 21:45:36 +0100

    add another xg plot

--- R/plot/plot-league-wfr.R
+++ R/plot/plot-league-wfr.R
@@ -21,7 +21,7 @@ plot_league_wfr <- function(data,season="2020-2021"){
     mutate(focus=case_when(
       key=="Goals" & min_rank(desc(n))<=10 ~ TRUE,
       key=="Expected Goals" & min_rank(desc(n))<=20 ~ TRUE,
-                           TRUE ~ FALSE)) %>%
+      TRUE ~ FALSE)) %>%
     ggplot(aes(x=0.05,y=n,alpha=focus)) +
     geom_text_repel(
       aes(label=ifelse(focus,Player,"")),
@@ -50,14 +50,14 @@ plot_league_wfr <- function(data,season="2020-2021"){
     unique() %>%
     filter(!is.na(PKatt)) %>%
     pivot_wider(names_from=Home_Away, values_from=PKatt, names_glue="PK_{Home_Away}")
-
+  
   plots$xg_for_against <-
     data$fbref$team_advanced_stats_match %>%
     filter(Season %in% !!season) %>%
     select(Match_Date,Home_Team,Away_Team,Home_xG,Away_xG,Team,Home_Away) %>%
     left_join(penalties) %>%
     mutate(Match_Date=lubridate::parse_date_time(Match_Date,"mdy")) %>%
-    filter(Match_Date>="2021-01-01") %>%
+    # filter(Match_Date>="2021-01-01") %>%
     mutate(Home_npxG=Home_xG-(0.7*PK_Home)) %>%
     mutate(Away_npxG=Away_xG-(0.7*PK_Away)) %>%
     mutate(Team_npxG=ifelse(Home_Away=="Home",Home_npxG,Away_npxG)) %>%
@@ -81,11 +81,39 @@ plot_league_wfr <- function(data,season="2020-2021"){
     theme[["solarfacet"]]() +
     facet_wrap("key",scales="free") +
     labs(
-      title="Expected Goals since 1 January 2021",
+      title="Expected Goals",
       x=element_blank(),
       y=element_blank()) +
     scale_x_continuous(limit=c(0,1)) +
     scale_y_continuous(breaks=seq(-100,100,5),labels=abs(seq(-100,100,5)),expand=expansion(add=1)) +
+    scale_fill_manual(values=palette[["epl"]]())
+  
+  plots$xg_for_against_sc <-
+    data$fbref$team_advanced_stats_match %>%
+    filter(Season %in% !!season) %>%
+    select(Match_Date,Home_Team,Away_Team,Home_xG,Away_xG,Team,Home_Away) %>%
+    left_join(penalties) %>%
+    mutate(Match_Date=lubridate::parse_date_time(Match_Date,"mdy")) %>%
+    # filter(Match_Date>="2021-01-01") %>%
+    mutate(Home_npxG=Home_xG-(0.7*PK_Home)) %>%
+    mutate(Away_npxG=Away_xG-(0.7*PK_Away)) %>%
+    mutate(Team_npxG=ifelse(Home_Away=="Home",Home_npxG,Away_npxG)) %>%
+    mutate(Opposition_npxG=ifelse(Home_Away=="Home",Away_npxG,Home_npxG)) %>%
+    select(Team,Team_npxG,Opposition_npxG) %>%
+    group_by(Team) %>%
+    summarise(across(where(is.numeric),sum,na.rm=TRUE)) %>%
+    ungroup() %>%
+    ggplot(aes(x=Team_npxG,y=Opposition_npxG)) +
+    geom_text_repel(aes(label=Team),size=2) +
+    geom_point(aes(fill=Team),shape=23,size=2.5) +
+    theme[["solar"]]() +
+    labs(
+      title="Expected goals",
+      x="xG for",
+      y="xG against"
+    ) +
+    scale_x_continuous(breaks=seq(0,100,5),expand=expansion(add=c(0.5))) +
+    scale_y_reverse(breaks=seq(0,100,5),expand=expansion(add=c(0.5))) +
     scale_fill_manual(values=palette[["epl"]]())
   
   plots_logo <-
