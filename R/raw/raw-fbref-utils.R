7afb0ed2869a947f740aa7342f13ef335a437acf
szfh <52595717+szfh@users.noreply.github.com>
2021-06-12 08:33:57 +0100

    rearrange

--- R/raw/raw-fbref-utils.R
+++ R/raw/raw-fbref-utils.R
@@ -1,3 +1,17 @@
+get_data_types <- function(){
+  data_types <- list()
+  data_types$season <- tibble(season=2018:2021)
+  data_types$country <- tibble(country="ENG")
+  # data_types$gender <- tibble(gender="M")
+  data_types$advanced_stats <- tibble(stat=c("summary","passing","passing_types","defense" ,"possession","misc","keeper"))
+  data_types$season_team_stats <- tibble(stat=c("league_table", "league_table_home_away", "standard", "keeper",
+                                                "keeper_adv", "shooting", "passing", "passing_types", "goal_shot_creation",
+                                                "defense", "possession", "playing_time", "misc"))
+  data_types$team_or_player <- tibble(team_or_player=c("player","team"))
+  
+  return(data_types)
+}
+
 # don't need stat and stat_key?
 fbref_get_selector <- function(page,season_key=NA,stat=NA,stat_key=NA,match_key=NA){
   
@@ -184,21 +198,6 @@ fbref_clean_names <- function(data,page,stat=NA){
     data %>%
     type_convert # refactor data types
 }
-
-get_data_types <- function(){
-  data_types <- list()
-  data_types$season <- tibble(season=2018:2021)
-  data_types$country <- tibble(country="ENG")
-  # data_types$gender <- tibble(gender="M")
-  data_types$advanced_stats <- tibble(stat=c("summary","passing","passing_types","defense" ,"possession","misc","keeper"))
-  data_types$season_team_stats <- tibble(stat=c("league_table", "league_table_home_away", "standard", "keeper",
-                                                "keeper_adv", "shooting", "passing", "passing_types", "goal_shot_creation",
-                                                "defense", "possession", "playing_time", "misc"))
-  data_types$team_or_player <- tibble(team_or_player=c("player","team"))
-  
-  return(data_types)
-}
-
 
 get_event_date <- function(data,data_type=NA,event_date=NA){
   
