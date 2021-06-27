ed95d24f4e8e8d4a0943a7c1a34ff327a6e15453
szfh <52595717+szfh@users.noreply.github.com>
2021-06-27 19:56:14 +0100

    add wfr sources

--- R/master.R
+++ R/master.R
@@ -4,6 +4,8 @@
 # raw
 # source(here("R","raw","raw-fbref.R"),encoding="utf-8")
 # scrape_fbref()
+# source(here("R","raw","raw-fbref-wfr.R"),encoding="utf-8")
+# scrape_fbref_wfr(save_path=here("data","fbref-wfr.rds"),current_season=2022)
 # source(here("R","raw","raw-understat.R"),encoding="utf-8")
 # scrape_understat()
 # source(here("R","raw","raw-canpl.R"),encoding="utf-8")
@@ -11,11 +13,17 @@
 
 # plot
 # source(here("R","join","join.R"),encoding="utf-8")
+# source(here("R","join","join-wfr.R"),encoding="utf-8")
+# j1 <- join_wfr(save_path_fbref=here("data","fbref-wfr.rds"))
 # source(here("R","themes.R"),encoding="utf-8")
 # source(here("R","plot","plot-team.R"),encoding="utf-8")
 # plot_team(data=join())
 # source(here("R","plot","plot-league.R"),encoding="utf-8")
 # plot_league(data=join())
+# source(here("R","plot","plot-team-wfr.R"),encoding="utf-8")
+# plot_team_wfr(data=join_wfr(save_path_fbref=here("data","fbref-wfr.rds")))
+# source(here("R","plot","plot-league-wfr.R"),encoding="utf-8")
+# plot_league_wfr(data=join_wfr(save_path_fbref=here("data","fbref-wfr.rds")))
 # source(here("R","plot","plot-cpl.R"),encoding="utf-8")
 # plot_cpl(data=join()["canpl"])
 
