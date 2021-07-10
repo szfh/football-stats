6406699db73400a1f02dbbfbb4cb4983312110b7
szfh <52595717+szfh@users.noreply.github.com>
2021-07-10 10:54:38 +0100

    update psxg to shots on target faced

--- R/plot/plot-team.R
+++ R/plot/plot-team.R
@@ -243,19 +243,20 @@ plot_team <- function(data,team="Southampton",season="2020-2021"){
     scale_colour_manual(values=c("TRUE"=colour[["sfc"]][["black"]],"FALSE"=colour[["sfc"]][["grey"]])) +
     scale_fill_manual(values=c("TRUE"=colour[["sfc"]][["light"]],"FALSE"=colour[["sfc"]][["grey"]]))
   
-  plots$psxg_against <-
+  plots$psxg_against_team <-
     data$fbref$advanced_stats_team_keeper %>%
     left_join(data$fbref$advanced_stats_team_misc) %>%
     mutate(Match_Date=parse_date_time(Match_Date,"mdy")) %>%
     filter(Season %in% !!season) %>%
-    select(Team,Match_Date,GA=GA_Shot_Stopping,psxG=PSxG_Shot_Stopping,OG) %>%
+    select(Team,Match_Date,GA=GA_Shot_Stopping,psxG=PSxG_Shot_Stopping,SoTA=SoTA_Shot_Stopping,OG) %>%
     mutate(psxGD=psxG-(GA-OG)) %>%
     arrange(Match_Date) %>%
     group_by(Team) %>%
     mutate(cumulative_psxGD=cumsum(psxGD)) %>%
+    mutate(cumulative_SoTA=cumsum(SoTA)) %>%
     ungroup() %>%
     mutate(focus=ifelse(Team %in% !!team,TRUE,FALSE)) %>%
-    ggplot(aes(x=Match_Date,y=cumulative_psxGD)) +
+    ggplot(aes(x=cumulative_SoTA,y=cumulative_psxGD)) +
     geom_path(aes(group=Team,alpha=focus,colour=focus),size=0.75) +
     theme[["solar"]]() +
     theme(
@@ -268,13 +269,15 @@ plot_team <- function(data,team="Southampton",season="2020-2021"){
     ) +
     labs(
       title=glue("GK expected saves - <b style='color:darkred'>{team}</b>"),
-      x=element_blank(),
+      x="Shots on target faced",
       y="Post-shot xG performance"
     ) +
-    scale_x_datetime(expand=expansion(add=100)) +
+    scale_x_continuous(expand=expansion(add=1)) +
     scale_y_continuous(expand=expansion(add=0.2)) +
     scale_colour_manual(values=c("TRUE"="darkred","FALSE"="darkgray")) +
     scale_alpha_manual(values=c("TRUE"=1,"FALSE"=0.5))
+  
+  # plots$psxg_against_player <-
   
   plots$passfootedness <-
     data$fbref$advanced_stats_player_passing_types %>%
