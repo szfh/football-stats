6486d5769196d56d37b69c41fdb1f92472f468e0
szfh <52595717+szfh@users.noreply.github.com>
2021-07-22 07:25:36 +0100

    add date scraped

--- R/raw/raw-understat.R
+++ R/raw/raw-understat.R
@@ -8,7 +8,7 @@ scrape_understat <- function(save_path=here("data","understat.rds"),current_seas
   understat_saved <- readRDS(save_path)
   understat <- list()
   
-  seasons <- tibble(season=as.character(2021))
+  seasons <- tibble(season=as.character(2020:2021))
   leagues <- tibble(league=c("EPL","La liga","Ligue 1","Bundesliga","Serie A"))
   
   understat$results$all <-
@@ -25,6 +25,7 @@ scrape_understat <- function(save_path=here("data","understat.rds"),current_seas
     anti_join(understat$results$all,understat$results$keep) %>%
     mutate(data=pmap(list(league,season),understat_league_match_results)) %>%
     mutate(data_type="results") %>%
+    mutate(date_scraped=Sys.Date()) %>%
     glimpse
   
   understat$shots$all <-
@@ -41,13 +42,18 @@ scrape_understat <- function(save_path=here("data","understat.rds"),current_seas
     anti_join(understat$shots$all,understat$shots$keep) %>%
     mutate(data=pmap(list(league,season),understat_league_season_shots)) %>%
     mutate(data_type="shots") %>%
+    mutate(date_scraped=Sys.Date()) %>%
     glimpse
   
   understat_all <-
     bind_rows(
       understat$results$keep,understat$results$new,
       understat$shots$keep,understat$shots$new
-    )
+    ) %>%
+    filter(!is.na(data)) %>%
+    relocate(data_type) %>%
+    relocate(date_scraped,.after=last_col()) %>%
+    relocate(data,.after=last_col())
   
   saveRDS(understat_all,file=save_path)
   
