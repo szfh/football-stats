f5b65ef20659812b43e87083502f42272d5858b9
szfh <52595717+szfh@users.noreply.github.com>
2021-12-02 21:13:05 +0000

    create team plots script

---
+++ R/plot/plot-new.R
@@ -0,0 +1,27 @@
+{
+  source(here::here("R","library.R"),encoding="utf-8")
+  source(here("R","join","join.R"),encoding="utf-8")
+  source(here("R","themes.R"),encoding="utf-8")
+  source(here("R","plot","plot-utils.R"),encoding="utf-8")
+}
+{
+  data <- join()
+}
+{
+  plots <- list()
+  team <- "Southampton"
+  season <- "2020-2021"
+  player <- ""
+}
+{
+  lapply(list.files(here("R","plot","team-plots"), full.names=TRUE), source, encoding="utf-8")
+  plots$minutes <- team_minutes(season,team)
+}
+{
+  plots_logo <-
+    plots %>%
+    add_logo(path=here("images","SB_Regular.png"),x=1,y=1,hjust=1.1,width=0.2) %>%
+    add_logo(path=here("images","fbref.png"),x=0.88,y=1,hjust=1.1,width=0.29)
+  
+  save_plots(plots_logo,path=here("plots","team"))
+}
