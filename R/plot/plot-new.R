9d49d4962d0c4473ae6778a0173150fd8afa26aa
szfh <52595717+szfh@users.noreply.github.com>
2021-12-03 21:32:06 +0000

    update layout

--- R/plot/plot-new.R
+++ R/plot/plot-new.R
@@ -10,11 +10,12 @@
 {
   plots <- list()
   team <- "Southampton"
-  season <- expand_seasons("2020-2021")
+  season <- expand_seasons("2021-2022")
   player <- ""
 }
 {
   lapply(list.files(here("R","plot","team-plots"), full.names=TRUE), source, encoding="utf-8")
+  plots <- list()
   plots$minutes <- team_minutes(team,season)
   plots$xg_trend <- xg_trend(team,season)
   plots$xg_segment <- xg_segment(team,season)
@@ -23,9 +24,17 @@
   plots$psxg_against <- psxg_against(team,season)
   plots$pass_footedness <- pass_footedness(team,season)
   plots$subs <- subs(team,season)
+  
+  plots_logo <-
+    plots %>%
+    add_logo(path=here("images","SB_Regular.png"),x=1,y=1,hjust=1.1,width=0.2) %>%
+    add_logo(path=here("images","fbref.png"),x=0.88,y=1,hjust=1.1,width=0.29)
+  
+  save_plots(plots_logo,path=here("plots","team"))
 }
 {
   lapply(list.files(here("R","plot","league-plots"), full.names=TRUE), source, encoding="utf-8")
+  plots <- list()
   plots$goals_xg_player <- goals_xg_player(season)
   plots$xg_xa_player <- xg_xa_player(season)
   plots$progressive_carries_player <- progressive_carries_player(season)
@@ -33,12 +42,11 @@
   plots$xg_team_scatter <- xg_team_scatter(season)
   plots$psxg_for <- psxg_for(season)
   plots$psxg_against <- psxg_against(season)
-}
-{
+  
   plots_logo <-
     plots %>%
     add_logo(path=here("images","SB_Regular.png"),x=1,y=1,hjust=1.1,width=0.2) %>%
     add_logo(path=here("images","fbref.png"),x=0.88,y=1,hjust=1.1,width=0.29)
   
-  save_plots(plots_logo,path=here("plots","team"))
+  save_plots(plots_logo,path=here("plots","league"))
 }
