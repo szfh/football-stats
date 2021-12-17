c79b7d4d506c80085a3e305446aeb46586ff76b3
szfh <52595717+szfh@users.noreply.github.com>
2021-12-17 13:46:39 +0000

    add per90

--- R/plot/league-plots/xg_team.R
+++ R/plot/league-plots/xg_team.R
@@ -1,4 +1,4 @@
-xg_team <- function(season,date=NA){
+xg_team <- function(season,per90=TRUE,date=NA){
   
   penalties <-
     data$fbref$advanced_stats_team_summary %>%
@@ -26,7 +26,10 @@ xg_team <- function(season,date=NA){
     ) %>%
     select(Team,Team_npxG,Opposition_npxG) %>%
     group_by(Team) %>%
-    summarise(across(where(is.numeric),sum,na.rm=TRUE),.groups="drop") %>%
+    {
+      if (per90) summarise(., across(where(is.numeric),mean,na.rm=TRUE),.groups="drop")
+      else summarise(., across(where(is.numeric),sum,na.rm=TRUE),.groups="drop")
+    } %>%
     make_long_data(levels=c("Team_npxG","Opposition_npxG"),labels=c("Expected Goals For","Expected Goals Against")) %>%
     ggplot(aes(x=0.05,y=n)) +
     geom_text_repel(
@@ -46,7 +49,9 @@ xg_team <- function(season,date=NA){
       x=element_blank(),
       y=element_blank()) +
     scale_x_continuous(limit=c(0,1)) +
-    scale_y_continuous(breaks=seq(-100,100,10),labels=abs(seq(-100,100,10)),expand=expansion(add=5)) +
+    # scale_y_continuous(breaks=seq(-100,100,10),labels=abs(seq(-100,100,10)),expand=expansion(mult=0.0.5)) +
+    scale_y_continuous(breaks=breaks_extended(8),expand=expansion(mult=c(0.05))) +
+    # scale_y_continuous(breaks=breaks_extended(8),labels=abs(breaks_extended(8)),expand=expansion(mult=c(0.05))) +
     scale_fill_manual(values=palette[["epl"]]())
   
   return(plot)
