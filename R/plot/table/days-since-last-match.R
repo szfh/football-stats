713a36b63031371ee1d8a0e44d0979035b7fae49
szfh <52595717+szfh@users.noreply.github.com>
2022-04-22 16:56:29 +0100

    better data input

--- R/plot/table/days-since-last-match.R
+++ R/plot/table/days-since-last-match.R
@@ -1,68 +1,56 @@
-days_since_last_match <- function(team){
+days_since_last_match <- function(team,season){
   
-  # data <- join(save_path_fbref=here("data","fbref.rds"))
-  # team_urls1819 <- worldfootballR::fb_teams_urls("https://fbref.com/en/comps/9/1889/")
-  # team_urls1920 <- worldfootballR::fb_teams_urls("https://fbref.com/en/comps/9/3232/")
-  team_urls2021 <- worldfootballR::fb_teams_urls("https://fbref.com/en/comps/9/10728/")
-  team_urls2122 <- worldfootballR::fb_teams_urls("https://fbref.com/en/comps/9/")
-  # team_urls <- worldfootballR::fb_teams_urls("https://fbref.com/en/comps/10/")
-  # team_urls <- worldfootballR::fb_teams_urls("https://fbref.com/en/comps/15/")
-  # team_urls <- worldfootballR::fb_teams_urls("https://fbref.com/en/comps/16/")
-  # fixtures1819 <- worldfootballR::get_team_match_results(team_urls1819)
-  # fixtures1920 <- worldfootballR::get_team_match_results(team_urls1920)
-  fixtures2021 <- worldfootballR::get_team_match_results(team_urls2021)
-  fixtures2122 <- worldfootballR::get_team_match_results(team_urls2122)
-  fixtures <- bind_rows(fixtures2021,fixtures2122)
-  # fixtures <- bind_rows(fixtures1819,fixtures1920,fixtures2021,fixtures2122)
+  season <- unique(expand_seasons(season))
   
-  league_fixtures <-
-    fixtures %>%
-    filter(Team==!!team) %>%
+  fixtures <-
+    data$fbref$match_results_allcomp %>%
+    filter(Season %in% !!season) %>%
+    filter(Team %in% !!team) %>%
+    filter(Result!="") %>%
     mutate(Date=as.Date(Date)) %>%
-    mutate(Date_previous=lag(Date,1),.after="Date") %>%
-    mutate(Days_since=as.integer(Date-Date_previous),.after="Date_previous") %>%
+    arrange(Date) %>%
+    mutate(Date_last_match=lag(Date,1),.after="Date") %>%
+    mutate(Days_between_matches=as.integer(Date-Date_last_match),.after="Date_last_match") %>%
+    mutate(Days_between_matches=case_when(
+      is.na(Days_between_matches) ~ 7,
+      Days_between_matches<3 ~ 3,
+      Days_between_matches>7 ~ 7,
+      TRUE ~ as.double(Days_between_matches))) %>%
+    filter(Comp=="Premier League") %>%
     mutate(across(c(GF,GA,xG,xGA),as.numeric)) %>%
     mutate(GD=GF-GA) %>%
-    mutate(xGD=xG-xGA) %>%
-    mutate(Days_since=case_when(
-      is.na(Days_since) ~ 7,
-      Days_since<3 ~ 3,
-      Days_since>7 ~ 7,
-      TRUE ~ as.double(Days_since))) %>%
-    filter(Comp=="Premier League") %>%
-    filter(!is.na(GF)) %>%
-    filter(Date>=as.Date("2018-12-07"))
+    mutate(xGD=xG-xGA)
   
   results <-
-    league_fixtures %>%
-    group_by(Days_since) %>%
+    fixtures %>%
+    group_by(Days_between_matches) %>%
     count(Result) %>%
     ungroup() %>%
     pivot_wider(names_from=Result,values_from=n) %>%
     replace_na(replace=list("L"=0,"D"=0,"W"=0)) %>%
-    mutate(MP=L+D+W) %>%
+    mutate(MP=L+D+W,.after="Days_between_matches") %>%
     mutate(Pts=(3*W)+D)
   
   goal_diff <-
-    league_fixtures %>%
-    select(-Poss,-Attendance) %>%
-    group_by(Days_since) %>%
+    fixtures %>%
+    select(-Season,-Poss,-Attendance) %>%
+    group_by(Days_between_matches) %>%
     summarise(across(where(is.numeric),sum,na.rm=TRUE),.groups="drop")
   
   results_by_days <-
     left_join(goal_diff,results) %>%
-    relocate(Days_since,MP) %>%
+    relocate(Days_between_matches,MP) %>%
     mutate(across(c(GF:xGD),~./MP)) %>%
     mutate(PPG=Pts/MP)
   
-  title <- glue("**{team} 2020-21 + 2021-22**")
+  title <- glue("**{team} {season[1]}**")
   
   table <-
     results_by_days %>%
     mutate(WDL=glue("{W}-{D}-{L}")) %>%
-    select(Days_since,P=MP,WDL,GD,xGD,PPG) %>%
-    mutate(Days_since=ifelse(Days_since==3,"2-3",as.character(Days_since))) %>%
-    mutate(Days_since=ifelse(Days_since==7,"7+",as.character(Days_since))) %>%
+    select(Days_between_matches,P=MP,WDL,GD,xGD,PPG) %>%
+    mutate(Days_between_matches=ifelse(Days_between_matches==3,"2-3",as.character(Days_between_matches))) %>%
+    mutate(Days_between_matches=ifelse(Days_between_matches==7,"7+",as.character(Days_between_matches))) %>%
     gt() %>%
     tab_header(
       title=md(title)) %>%
@@ -75,7 +63,7 @@ days_since_last_match <- function(team){
       column_labels.border.bottom.width= px(3)
     ) %>%
     cols_label(
-      Days_since = md("Days since<br>last match"),
+      Days_between_matches = md("Days since<br>last match"),
       WDL = md("W-D-L")
     ) %>%
     fmt_number(columns=c(4:6),decimals=2) %>%
@@ -87,7 +75,7 @@ days_since_last_match <- function(team){
       ),
       locations = list(
         cells_title("title"),
-        cells_column_labels(-"Days_since"),
+        cells_column_labels(-"Days_between_matches"),
         cells_body()
       )
     ) %>%
@@ -96,7 +84,7 @@ days_since_last_match <- function(team){
         font = google_font("Open Sans"),
         size = "small"
       ),
-      locations = list(cells_column_labels("Days_since"))
+      locations = list(cells_column_labels("Days_between_matches"))
     ) %>%
     tab_style(
       style = list(
