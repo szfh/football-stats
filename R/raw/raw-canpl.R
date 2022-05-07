0f20008afddb50e8acbcdec59837c3e01d08e975
szfh <52595717+szfh@users.noreply.github.com>
2022-05-07 12:05:30 +0100

    fix canpl for new folder structure

--- R/raw/raw-canpl.R
+++ R/raw/raw-canpl.R
@@ -1,22 +1,27 @@
 import_canpl <- function(save_folder=here("data","CPL"),save_path=here("data","canpl.rds")){
   
-  find_cc <- drive_find(pattern="Centre Circle Data & Info") #auto select account?
-  get_cc <- drive_get(as_id(find_cc[1,]))
+  centre_circle_data_folders <-
+    drive_ls(pattern="Centre Circle Data") %>%
+    drive_ls() %>%
+    filter(str_detect(name,"Season"))
   
-  csv_files <- drive_ls(get_cc, type = "csv") %>%
-    mutate(path=glue({"{save_folder}/{name}"}))
-  
-  for(i in 1:dim(csv_files)[1]){
-    drive_download(file=csv_files[i,],path=as.character(csv_files[i,"path"]),overwrite=TRUE)
+  for(i in 1:dim(centre_circle_data_folders)[1]){
+    csv_files <-
+      centre_circle_data_folders %>%
+      slice(i) %>%
+      drive_ls(path=., type="csv") %>%
+      mutate(path=glue("{save_folder}/{name}"))
+    
+    for(j in 1:dim(csv_files)[1]){
+      drive_download(file=csv_files[j,],path=as.character(csv_files[j,"path"]),overwrite=TRUE)
+    }
   }
   
-  csv_local <- dir(path=save_folder,pattern=".csv")
+  csv_local_files <- dir(path=save_folder,pattern=".csv")
   
-  canpl <- tibble(name=csv_local) %>%
-    mutate(path=glue("{save_folder}/{csv_local}")) %>%
-    mutate(data=map(path,read_csv))
+  canpl <- tibble(name=csv_local_files) %>%
+    mutate(data=map(glue("{save_folder}/{csv_local_files}"),read_csv))
   
   saveRDS(canpl,file=save_path)
-  
   return(canpl)
 }
