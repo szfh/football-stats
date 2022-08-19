8bbca99bb86907767343af0ba7923ec1630fce26
szfh <52595717+szfh@users.noreply.github.com>
2022-08-19 13:28:08 +0100

    update

--- R/plot/team-plots/xg_trend.R
+++ R/plot/team-plots/xg_trend.R
@@ -1,7 +1,7 @@
 xg_trend <- function(team,season,lastn=NA,since=NA){
   penalties <-
     data$fbref$advanced_stats_team_summary %>%
-    mutate(Match_Date=parse_date_time(Match_Date,"mdy")) %>%
+    mutate(Match_Date=parse_date_time(Match_Date,"ymd")) %>%
     filter(!is.na(PKatt)) %>%
     filter((Home_Team %in% !!team)|(Away_Team %in% !!team)) %>%
     select(Match_Date,Home_Away,PKatt) %>%
@@ -11,8 +11,8 @@ xg_trend <- function(team,season,lastn=NA,since=NA){
     data$fbref$advanced_stats_team_summary %>%
     filter(Team %in% !!team) %>%
     filter((!is.na(Home_xG))|!is.na(Away_xG)) %>%
-    mutate(Match_Date=parse_date_time(Match_Date,"mdy")) %>%
-    select(Season,Match_Date,Team,Home_Team,Home_Score,Home_xG,Away_Team,Away_Score,Away_xG,Home_Away) %>%
+    mutate(Match_Date=parse_date_time(Match_Date,"ymd")) %>%
+    select(season,Match_Date,Team,Home_Team,Home_Score,Home_xG,Away_Team,Away_Score,Away_xG,Home_Away) %>%
     left_join(penalties) %>%
     arrange(Match_Date) %>%
     mutate(
@@ -33,21 +33,21 @@ xg_trend <- function(team,season,lastn=NA,since=NA){
     mutate(
       Team_npxG_mva=get_mva(Team_npxG),
       Opposition_npxG_mva=get_mva(Opposition_npxG)) %>%
-    filter(Season %in% !!season) %>%
-    mutate(Season=case_when(
-      Match_Date>=as.Date("2019-08-01") & Match_Date<as.Date("2020-04-01") ~ "2019-20 part 1",
-      Match_Date>=as.Date("2020-04-01") & Match_Date<as.Date("2020-08-01") ~ "2019-20 part 2",
-      TRUE ~ Season)) %>%
+    filter(season %in% !!season) %>%
+    # mutate(season=case_when(
+    #   Match_Date>=as.Date("2019-08-01") & Match_Date<as.Date("2020-04-01") ~ "2019-20 part 1",
+    #   Match_Date>=as.Date("2020-04-01") & Match_Date<as.Date("2020-08-01") ~ "2019-20 part 2",
+    #   TRUE ~ season)) %>%
     {if (!is.na(since)) filter(., Match_Date>=as.Date(since)) else .} %>%
     {if (!is.na(lastn)) slice_tail(., n=lastn) else .} %>%
     mutate(Home_Away_Short=ifelse(Home_Away=="Home","H","A"),.after="Home_Away") %>%
     mutate(Match=glue::glue("{Opposition} {Home_Away_Short} {Team_Score}-{Opposition_Score}")) %>%
-    mutate(Match=reorder_within(Match, Match_Date, Season)) %>%
+    mutate(Match=reorder_within(Match, Match_Date, season)) %>%
     ggplot(aes(x=Match)) +
     geom_point(aes(y=Team_npxG),size=1,colour="darkred",fill="darkred",alpha=0.5,shape=23) +
-    geom_line(aes(y=Team_npxG_mva,group=Season),colour="darkred",linetype="longdash",size=0.7) +
+    geom_line(aes(y=Team_npxG_mva,group=season),colour="darkred",linetype="longdash",size=0.7) +
     geom_point(aes(y=Opposition_npxG),size=1,colour="royalblue",fill="royalblue",alpha=0.5,shape=23) +
-    geom_line(aes(y=Opposition_npxG_mva,group=Season),colour="royalblue",linetype="longdash",size=0.7) +
+    geom_line(aes(y=Opposition_npxG_mva,group=season),colour="royalblue",linetype="longdash",size=0.7) +
     theme[["solar"]]() +
     theme(
       axis.text.x=element_text(size=6,angle=60,hjust=1),
@@ -64,7 +64,7 @@ xg_trend <- function(team,season,lastn=NA,since=NA){
     ) +
     scale_x_reordered() +
     scale_y_continuous(limits=c(0,NA),breaks=seq(0,10,1),expand=expansion(add=c(0,0.1))) +
-    facet_grid(cols=vars(Season), space="free", scales="free_x")
+    facet_grid(cols=vars(season), space="free", scales="free_x")
   
   return(plot)
 }
