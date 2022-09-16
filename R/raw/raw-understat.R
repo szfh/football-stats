c3f59110028763a194f6debef8768eaafcbbda59
szfh <52595717+szfh@users.noreply.github.com>
2022-09-16 16:13:06 +0100

    get understat team stats

--- R/raw/raw-understat.R
+++ R/raw/raw-understat.R
@@ -11,6 +11,7 @@ scrape_understat <- function(
   data_types <- get_data_types()
   
   understat_results_saved <- readRDS(save_path_results)
+  understat_team_stats_saved <- readRDS(save_path_team_stats)
   understat_shots_saved <- readRDS(save_path_shots)
   understat <- list()
   
@@ -33,7 +34,44 @@ scrape_understat <- function(
     mutate(data=pmap(list(league,season),understat_league_match_results)) %>%
     mutate(data_type="results") %>%
     mutate(date_scraped=Sys.Date()) %>%
-    glimpse
+    print(n=Inf)
+  
+  understat$team_meta$all <-
+    bind_rows(understat$results$keep,understat$results$new) %>%
+    select(data) %>%
+    unnest(cols=data) %>%
+    select(league,home_team,away_team) %>%
+    pivot_longer(cols=c("home_team","away_team"),values_to="team",names_to=NULL) %>%
+    unique() %>%
+    arrange(league,team)
+  
+  understat$team_meta$keep <-
+    understat_team_stats_saved %>%
+    filter(data_type=="team_meta")
+  
+  understat$team_meta$new <-
+    anti_join(understat$team_meta$all,understat$team_meta$keep) %>%
+    mutate(data=pmap(list(team),understat_team_meta)) %>%
+    mutate(data_type="team_meta") %>%
+    mutate(date_scraped=Sys.Date()) %>%
+    print(n=Inf)
+  
+  understat$team_stats$all <-
+    bind_rows(understat$team_meta$keep,understat$team_meta$new) %>%
+    select(data) %>%
+    unnest(data) %>%
+    rename(team=team_name)
+  
+  understat$team_stats$keep <-
+    understat_team_stats_saved %>%
+    filter(data_type=="team_stats")
+  
+  understat$team_stats$new <-
+    anti_join(understat$team_stats$all,understat$team_stats$keep) %>%
+    mutate(data=pmap(list(url),understat_team_stats_breakdown)) %>%
+    mutate(data_type="team_stats") %>%
+    mutate(date_scraped=Sys.Date()) %>%
+    print(n=Inf)
   
   understat$shots <-
     worldfootballR::load_understat_league_shots("EPL")
@@ -47,10 +85,21 @@ scrape_understat <- function(
     relocate(date_scraped,.after=last_col()) %>%
     relocate(data,.after=last_col())
   
+  understat_team_stats <-
+    bind_rows(
+      understat$team_meta$keep,understat$team_meta$new,
+      understat$team_stats$keep,understat$team_stats$new
+    ) %>%
+    filter(!is.na(data)) %>%
+    relocate(data_type) %>%
+    relocate(date_scraped,.after=last_col()) %>%
+    relocate(data,.after=last_col())
+  
   understat_shots <-
     understat$shots
   
   saveRDS(understat_results,file=save_path_results)
+  saveRDS(understat_team_stats,file=save_path_team_stats)
   saveRDS(understat_shots,file=save_path_shots)
   
   return()
